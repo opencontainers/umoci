28246675f11c9b6b2b193d01af7a8939b0576593
Aleksa Sarai <asarai@suse.com>
2016-12-17 11:13:23 +1100

    *: add L*xattr to FsEval
    Signed-off-by: Aleksa Sarai <asarai@suse.com>

--- fseval.go
+++ fseval.go
@@ -33,47 +33,62 @@ var _ mtree.FsEval = RootlessFsEval
 // mtree.FsEval as well as including all of the imporant os.* wrapper functions
 // needed for "oci/layers".tarExtractor.
 type FsEval interface {
-	// Open is a wrapper around unpriv.Open.
+	// Open is equivalent to os.Open.
 	Open(path string) (*os.File, error)
 
-	// Create is a wrapper around unpriv.Create.
+	// Create is equivalent to os.Create.
 	Create(path string) (*os.File, error)
 
-	// Readdir is a wrapper around unpriv.Readdir.
+	// Readdir is equivalent to os.Readdir.
 	Readdir(path string) ([]os.FileInfo, error)
 
-	// Lstat is a wrapper around unpriv.Lstat.
+	// Lstat is equivalent to os.Lstat.
 	Lstat(path string) (os.FileInfo, error)
 
-	// Readlink is a wrapper around unpriv.Readlink.
+	// Readlink is equivalent to os.Readlink.
 	Readlink(path string) (string, error)
 
-	// Symlink is a wrapper around unpriv.Symlink.
+	// Symlink is equivalent to os.Symlink.
 	Symlink(linkname, path string) error
 
-	// Link is a wrapper around unpriv.Link.
+	// Link is equivalent to os.Link.
 	Link(linkname, path string) error
 
-	// Chmod is a wrapper around unpriv.Chmod.
+	// Chmod is equivalent to os.Chmod.
 	Chmod(path string, mode os.FileMode) error
 
-	// Lutimes is a wrapper around unpriv.Lutimes.
+	// Lutimes is equivalent to os.Lutimes.
 	Lutimes(path string, atime, mtime time.Time) error
 
-	// Remove is a wrapper around unpriv.Remove.
+	// Remove is equivalent to os.Remove.
 	Remove(path string) error
 
-	// RemoveAll is a wrapper around unpriv.RemoveAll.
+	// RemoveAll is equivalent to os.RemoveAll.
 	RemoveAll(path string) error
 
-	// Mkdir is a wrapper around unpriv.Mkdir.
+	// Mkdir is equivalent to os.Mkdir.
 	Mkdir(path string, perm os.FileMode) error
 
-	// MkdirAll is a wrapper around unpriv.MkdirAll.
+	// MkdirAll is equivalent to os.MkdirAll.
 	MkdirAll(path string, perm os.FileMode) error
 
 	// Mknod is equivalent to system.Mknod.
 	Mknod(path string, mode os.FileMode, dev system.Dev_t) error
+
+	// Llistxattr is equivalent to system.Llistxattr
+	Llistxattr(path string) ([]string, error)
+
+	// Lremovexattr is equivalent to system.Lremovexattr
+	Lremovexattr(path, name string) error
+
+	// Lsetxattr is equivalent to system.Lsetxattr
+	Lsetxattr(path, name string, value []byte, flags int) error
+
+	// Lgetxattr is equivalent to system.Lgetxattr
+	Lgetxattr(path string, name string) ([]byte, error)
+
+	// Lclearxattrs is equivalent to system.Lclearxattrs
+	Lclearxattrs(path string) error
 
 	// KeywordFunc returns a wrapper around the given mtree.KeywordFunc.
 	KeywordFunc(fn mtree.KeywordFunc) mtree.KeywordFunc
