opencontainers/umoci

commit 8b73b492fdf18ac6b90cc972b01ab8bb79f36f76

*: make logging [log.Info] much more user-friendly
This is necessary to make it more obvious to users what is going on.

Signed-off-by: Aleksa Sarai <asarai@suse.de>

cmd/umoci/tag.go
@@ -20,6 +20,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/apex/log"
 	"github.com/cyphar/umoci/oci/cas"
 	"github.com/pkg/errors"
 	"github.com/urfave/cli"
@@ -77,6 +78,7 @@ func tagAdd(ctx *cli.Context) error {
 		return errors.Wrap(err, "put reference")
 	}
 
+	log.Infof("created new tag: %q -> %q", tagName, fromName)
 	return nil
 }
 
@@ -112,6 +114,7 @@ func tagRemove(ctx *cli.Context) error {
 		return errors.Wrap(err, "delete reference")
 	}
 
+	log.Infof("removed tag: %s", tagName)
 	return nil
 }
 

cmd/umoci/config.go
@@ -279,21 +279,23 @@ func config(ctx *cli.Context) error {
 		return errors.Wrap(err, "commit mutated image")
 	}
 
-	log.WithFields(log.Fields{
-		"mediatype": newDescriptor.MediaType,
-		"digest":    newDescriptor.Digest,
-		"size":      newDescriptor.Size,
-	}).Infof("created new image")
+	log.Infof("new image manifest created: %s", newDescriptor.Digest)
 
-	// We have to clobber the old reference.
-	// XXX: Should we output some warning if we actually did remove an old
-	//      reference?
-	if err := engine.DeleteReference(context.Background(), tagName); err != nil {
-		return errors.Wrap(err, "delete old tag")
+	err = engine.PutReference(context.Background(), tagName, &newDescriptor)
+	if err == cas.ErrClobber {
+		// We have to clobber a tag.
+		log.Warnf("clobbering existing tag: %s", tagName)
+
+		// Delete the old tag.
+		if err := engine.DeleteReference(context.Background(), tagName); err != nil {
+			return errors.Wrap(err, "delete old tag")
+		}
+		err = engine.PutReference(context.Background(), tagName, &newDescriptor)
 	}
-	if err := engine.PutReference(context.Background(), tagName, &newDescriptor); err != nil {
+	if err != nil {
 		return errors.Wrap(err, "add new tag")
 	}
 
+	log.Infof("created new tag for image manifest: %s", tagName)
 	return nil
 }

cmd/umoci/init.go
@@ -58,8 +58,6 @@ func initLayout(ctx *cli.Context) error {
 		return errors.Wrap(err, "image layout creation")
 	}
 
-	log.WithFields(log.Fields{
-		"path": imagePath,
-	}).Infof("created new OCI image layout")
+	log.Infof("created new OCI image: %s", imagePath)
 	return nil
 }

cmd/umoci/main.go
@@ -44,6 +44,7 @@ const (
 )
 
 func main() {
+	// TODO: Add some form of --verbose flag.
 	log.SetHandler(logcli.New(os.Stderr))
 
 	app := cli.NewApp()
@@ -138,7 +139,7 @@ func main() {
 		// that --rootless might help. We probably should only be doing this if
 		// we're an unprivileged user.
 		if os.IsPermission(errors.Cause(err)) {
-			log.Infof("umoci encountered a permission error -- maybe --rootless will help?")
+			log.Info("umoci encountered a permission error: maybe --rootless will help?")
 		}
 		log.Fatalf("%v", err)
 	}

cmd/umoci/new.go
@@ -64,7 +64,7 @@ func newImage(ctx *cli.Context) error {
 	// Create a new manifest.
 	log.WithFields(log.Fields{
 		"tag": tagName,
-	}).Infof("creating new manifest")
+	}).Debugf("creating new manifest")
 
 	// Create a new image config.
 	g := igen.New()
@@ -127,21 +127,24 @@ func newImage(ctx *cli.Context) error {
 		Size:      manifestSize,
 	}
 
-	log.WithFields(log.Fields{
-		"mediatype": descriptor.MediaType,
-		"digest":    descriptor.Digest,
-		"size":      descriptor.Size,
-	}).Infof("created new image")
+	log.Infof("new image manifest created: %s", descriptor.Digest)
 
-	// We have to clobber the old reference.
-	// XXX: Should we output some warning if we actually did remove an old
-	//      reference?
-	if err := engine.DeleteReference(context.Background(), tagName); err != nil {
-		return errors.Wrap(err, "delete old tag")
+	err = engine.PutReference(context.Background(), tagName, &descriptor)
+	if err == cas.ErrClobber {
+		// We have to clobber a tag.
+		log.Warnf("clobbering existing tag: %s", tagName)
+
+		// Delete the old tag.
+		if err := engine.DeleteReference(context.Background(), tagName); err != nil {
+			return errors.Wrap(err, "delete old tag")
+		}
+		err = engine.PutReference(context.Background(), tagName, &descriptor)
 	}
-	if err := engine.PutReference(context.Background(), tagName, &descriptor); err != nil {
+	if err != nil {
 		return errors.Wrap(err, "add new tag")
 	}
+
+	log.Infof("created new tag for image manifest: %s", tagName)
 
 	return nil
 }

cmd/umoci/repack.go
@@ -144,10 +144,12 @@ func repack(ctx *cli.Context) error {
 		fsEval = umoci.RootlessFsEval
 	}
 
+	log.Info("computing filesystem diff ...")
 	diffs, err := mtree.Check(fullRootfsPath, spec, MtreeKeywords, fsEval)
 	if err != nil {
 		return errors.Wrap(err, "check mtree")
 	}
+	log.Info("... done")
 
 	log.WithFields(log.Fields{
 		"ndiff": len(diffs),
@@ -196,21 +198,23 @@ func repack(ctx *cli.Context) error {
 		return errors.Wrap(err, "commit mutated image")
 	}
 
-	log.WithFields(log.Fields{
-		"mediatype": newDescriptor.MediaType,
-		"digest":    newDescriptor.Digest,
-		"size":      newDescriptor.Size,
-	}).Infof("created new image")
+	log.Infof("new image manifest created: %s", newDescriptor.Digest)
 
-	// We have to clobber the old reference.
-	// XXX: Should we output some warning if we actually did remove an old
-	//      reference?
-	if err := engine.DeleteReference(context.Background(), tagName); err != nil {
-		return err
+	err = engine.PutReference(context.Background(), tagName, &newDescriptor)
+	if err == cas.ErrClobber {
+		// We have to clobber a tag.
+		log.Warnf("clobbering existing tag: %s", tagName)
+
+		// Delete the old tag.
+		if err := engine.DeleteReference(context.Background(), tagName); err != nil {
+			return errors.Wrap(err, "delete old tag")
+		}
+		err = engine.PutReference(context.Background(), tagName, &newDescriptor)
 	}
-	if err := engine.PutReference(context.Background(), tagName, &newDescriptor); err != nil {
-		return err
+	if err != nil {
+		return errors.Wrap(err, "add new tag")
 	}
 
+	log.Infof("created new tag for image manifest: %s", tagName)
 	return nil
 }

cmd/umoci/unpack.go
@@ -94,15 +94,9 @@ func unpack(ctx *cli.Context) error {
 	if meta.MapOptions.Rootless {
 		if !ctx.IsSet("uid-map") {
 			ctx.Set("uid-map", fmt.Sprintf("%d:0:1", os.Geteuid()))
-			log.WithFields(log.Fields{
-				"map.uid": ctx.StringSlice("uid-map"),
-			}).Info("setting default rootless --uid-map option")
 		}
 		if !ctx.IsSet("gid-map") {
 			ctx.Set("gid-map", fmt.Sprintf("%d:0:1", os.Getegid()))
-			log.WithFields(log.Fields{
-				"map.gid": ctx.StringSlice("gid-map"),
-			}).Info("setting default rootless --gid-map option")
 		}
 	}
 	// Parse and set up the mapping options.
@@ -120,10 +114,11 @@ func unpack(ctx *cli.Context) error {
 		}
 		meta.MapOptions.GIDMappings = append(meta.MapOptions.GIDMappings, idMap)
 	}
+
 	log.WithFields(log.Fields{
 		"map.uid": meta.MapOptions.UIDMappings,
 		"map.gid": meta.MapOptions.GIDMappings,
-	}).Infof("parsed mappings")
+	}).Debugf("parsed mappings")
 
 	// Get a reference to the CAS.
 	engine, err := cas.Open(imagePath)
@@ -172,9 +167,11 @@ func unpack(ctx *cli.Context) error {
 	// FIXME: Currently we only support OCI layouts, not tar archives. This
 	//        should be fixed once the CAS engine PR is merged into
 	//        image-tools. https://github.com/opencontainers/image-tools/pull/5
+	log.Info("unpacking bundle ...")
 	if err := layer.UnpackManifest(context.Background(), engine, bundlePath, *manifest, &meta.MapOptions); err != nil {
 		return errors.Wrap(err, "create runtime bundle")
 	}
+	log.Info("... done")
 
 	log.WithFields(log.Fields{
 		"keywords": MtreeKeywords,
@@ -186,10 +183,12 @@ func unpack(ctx *cli.Context) error {
 		fsEval = umoci.RootlessFsEval
 	}
 
+	log.Info("computing filesystem manifest ...")
 	dh, err := mtree.Walk(fullRootfsPath, nil, MtreeKeywords, fsEval)
 	if err != nil {
 		return errors.Wrap(err, "generate mtree spec")
 	}
+	log.Info("... done")
 
 	fh, err := os.OpenFile(mtreePath, os.O_EXCL|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
@@ -213,6 +212,6 @@ func unpack(ctx *cli.Context) error {
 		return errors.Wrap(err, "write umoci.json metadata")
 	}
 
-	log.Debugf("umoci: unpacking complete")
+	log.Infof("unpacked image bundle: %s", bundlePath)
 	return nil
 }

oci/cas/gc.go
@@ -223,9 +223,8 @@ func GC(ctx context.Context, engine Engine) error {
 			// Digest is in the black set.
 			continue
 		}
-		log.WithFields(log.Fields{
-			"digest": digest,
-		}).Infof("GC: garbage collecting blob")
+		log.Infof("garbage collecting blob: %s", digest)
+
 		if err := engine.DeleteBlob(ctx, digest); err != nil {
 			return errors.Wrapf(err, "remove unmarked blob %s", digest)
 		}
@@ -237,6 +236,6 @@ func GC(ctx context.Context, engine Engine) error {
 		return errors.Wrapf(err, "GC engine")
 	}
 
-	log.Infof("GC: garbage collected %d blobs", n)
+	log.Debugf("garbage collected %d blobs", n)
 	return nil
 }

commit 870827399426d6b6810d5f38c11fcb2d24ceb3dd

cmd/umoci: tag: fix tag clobbering
Previously, tags would always be clobbered, but this logic was subtly
broken for "umoci tag". Fix this by matching the other PutReference
usages.

Fixes: 8b73b492fdf1 ("*: make logging [log.Info] much more user-friendly")
Signed-off-by: Aleksa Sarai <asarai@suse.de>

cmd/umoci/tag.go
@@ -74,7 +74,18 @@ func tagAdd(ctx *cli.Context) error {
 	}
 
 	// Add it.
-	if err := engine.PutReference(context.Background(), tagName, descriptor); err != nil {
+	err = engine.PutReference(context.Background(), tagName, descriptor)
+	if err == cas.ErrClobber {
+		// We have to clobber a tag.
+		log.Warnf("clobbering existing tag: %s", tagName)
+
+		// Delete the old tag.
+		if err := engine.DeleteReference(context.Background(), tagName); err != nil {
+			return errors.Wrap(err, "delete old tag")
+		}
+		err = engine.PutReference(context.Background(), tagName, descriptor)
+	}
+	if err != nil {
 		return errors.Wrap(err, "put reference")
 	}
 