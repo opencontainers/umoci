53af7fb0aa12130a046a3079bde6d8b4f242cb75
Aleksa Sarai <asarai@suse.de>
2017-07-20 03:15:59 +1000

    vendor: add github.com/cyphar/filepath-securejoin@v0.2.1
    This acts as a replacement for our fork of Docker's FollowSymlinkInScope
    in that the API has been massively improved to be more useful.
    
    Signed-off-by: Aleksa Sarai <asarai@suse.de>

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -130,6 +130,7 @@ clone github.com/docker/go-units v0.3.1
 clone github.com/pkg/errors v0.8.0
 clone github.com/apex/log afb2e76037a5f36542c77e88ef8aef9f469b09f8
 clone github.com/urfave/cli v1.18.1
+clone github.com/cyphar/filepath-securejoin v0.2.1
 clone github.com/vbatts/go-mtree 711a89aa4c4a8f148d87eb915456eba8ee7c6a0b
 clone golang.org/x/net 45e771701b814666a7eb299e6c7a57d0b1799e91 https://github.com/golang/net
 # Used purely for testing.
