opencontainers/umoci

commit 9f88dd9a114d5bb3a3b87c5be6f00ea1a06d61c6

{un,re}pack: implement ID mapping
This only includes the required ID mapping code for a root user to unmap
to any other user range. While very useful, this doesn't help with
rootless containers and users. That'll require some more work.

Implements: cyphar/umoci#32
Signed-off-by: Aleksa Sarai <asarai@suse.com>

cmd/umoci/unpack.go
@@ -26,6 +26,7 @@ import (
 	"github.com/Sirupsen/logrus"
 	"github.com/cyphar/umoci/image/cas"
 	"github.com/cyphar/umoci/image/layer"
+	"github.com/cyphar/umoci/pkg/idtools"
 	"github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/urfave/cli"
 	"github.com/vbatts/go-mtree"
@@ -58,6 +59,14 @@ creation with umoci-repack(1).`,
 		cli.StringFlag{
 			Name:  "bundle",
 			Usage: "destination bundle path",
+		},
+		cli.StringSliceFlag{
+			Name:  "uid-map",
+			Usage: "specifies a uid mapping to use when repacking",
+		},
+		cli.StringSliceFlag{
+			Name:  "gid-map",
+			Usage: "specifies a gid mapping to use when repacking",
 		},
 	},
 
@@ -97,6 +106,27 @@ func unpack(ctx *cli.Context) error {
 	if fromName == "" {
 		return fmt.Errorf("reference name cannot be empty")
 	}
+
+	// Parse map options.
+	mapOptions := layer.MapOptions{}
+	for _, uidmap := range ctx.StringSlice("uid-map") {
+		idMap, err := idtools.ParseMapping(uidmap)
+		if err != nil {
+			return fmt.Errorf("failure parsing --uid-map %s: %s", uidmap, err)
+		}
+		mapOptions.UIDMappings = append(mapOptions.UIDMappings, idMap)
+	}
+	for _, gidmap := range ctx.StringSlice("gid-map") {
+		idMap, err := idtools.ParseMapping(gidmap)
+		if err != nil {
+			return fmt.Errorf("failure parsing --gid-map %s: %s", gidmap, err)
+		}
+		mapOptions.GIDMappings = append(mapOptions.GIDMappings, idMap)
+	}
+	logrus.WithFields(logrus.Fields{
+		"map.uid": mapOptions.UIDMappings,
+		"map.gid": mapOptions.GIDMappings,
+	}).Infof("parsed mappings")
 
 	// Get a reference to the CAS.
 	engine, err := cas.Open(imagePath)
@@ -153,7 +183,7 @@ func unpack(ctx *cli.Context) error {
 	//        extraction). I'm considering reimplementing it just so there are
 	//        competing implementations of this extraction functionality.
 	//           https://github.com/opencontainers/image-tools/issues/74
-	if err := layer.UnpackManifest(context.TODO(), engine, bundlePath, *manifest); err != nil {
+	if err := layer.UnpackManifest(context.TODO(), engine, bundlePath, *manifest, &mapOptions); err != nil {
 		return fmt.Errorf("failed to create runtime bundle: %q", err)
 	}
 

pkg/idtools/idtools.go
@@ -1,0 +1,98 @@
+/*
+ * umoci: Umoci Modifies Open Containers' Images
+ * Copyright (C) 2016 SUSE LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package idtools
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	rspec "github.com/opencontainers/runtime-spec/specs-go"
+)
+
+// ToHost translates a remapped container ID to an unmapped host ID using the
+// provided ID mapping. If no mapping is provided, then the mapping is a no-op.
+// If there is no mapping for the given ID an error is returned.
+func ToHost(contID int, idMap []rspec.IDMapping) (int, error) {
+	if idMap == nil {
+		return contID, nil
+	}
+
+	for _, m := range idMap {
+		if uint32(contID) >= m.ContainerID && uint32(contID) < m.ContainerID+m.Size {
+			return int(m.HostID + (uint32(contID) - m.ContainerID)), nil
+		}
+	}
+
+	return -1, fmt.Errorf("container id %d cannot be mapped to a host id", contID)
+}
+
+// ToContainer takes an unmapped host ID and translates it to a remapped
+// container ID using the provided ID mapping. If no mapping is provided, then
+// the mapping is a no-op. If there is no mapping for the given ID an error is
+// returned.
+func ToContainer(hostID int, idMap []rspec.IDMapping) (int, error) {
+	if idMap == nil {
+		return hostID, nil
+	}
+
+	for _, m := range idMap {
+		if uint32(hostID) >= m.HostID && uint32(hostID) < m.HostID+m.Size {
+			return int(m.ContainerID + (uint32(hostID) - m.HostID)), nil
+		}
+	}
+
+	return -1, fmt.Errorf("host id %d cannot be mapped to a container id", hostID)
+}
+
+// ParseMapping takes a mapping string of the form "host:container[:size]" and
+// returns the corresponding rspec.IDMapping. An error is returned if not
+// enough fields are provided or are otherwise invalid. The default size is 1.
+func ParseMapping(spec string) (rspec.IDMapping, error) {
+	parts := strings.Split(spec, ":")
+
+	var err error
+	var hostID, contID, size int
+	switch len(parts) {
+	case 3:
+		size, err = strconv.Atoi(parts[2])
+		if err != nil {
+			return rspec.IDMapping{}, fmt.Errorf("invalid size field: %s", err)
+		}
+	case 2:
+		size = 1
+	default:
+		return rspec.IDMapping{}, fmt.Errorf("invalid number of fields in mapping '%s': %d", spec, len(parts))
+	}
+
+	hostID, err = strconv.Atoi(parts[0])
+	if err != nil {
+		return rspec.IDMapping{}, fmt.Errorf("invalid hostID field: %s", err)
+	}
+
+	contID, err = strconv.Atoi(parts[1])
+	if err != nil {
+		return rspec.IDMapping{}, fmt.Errorf("invalid containerID field: %s", err)
+	}
+
+	return rspec.IDMapping{
+		HostID:      uint32(hostID),
+		ContainerID: uint32(contID),
+		Size:        uint32(size),
+	}, nil
+}

image/layer/unpack.go
@@ -30,6 +30,7 @@ import (
 	"github.com/Sirupsen/logrus"
 	"github.com/cyphar/umoci/image/cas"
 	igen "github.com/cyphar/umoci/image/generator"
+	"github.com/cyphar/umoci/pkg/idtools"
 	"github.com/cyphar/umoci/system"
 	"github.com/opencontainers/image-spec/specs-go/v1"
 	rgen "github.com/opencontainers/runtime-tools/generate"
@@ -40,8 +41,12 @@ import (
 // root. It ensures that the state of the root is as close as possible to the
 // state used to create the layer. If an error is returned, the state of root
 // is undefined (unpacking is not guaranteed to be atomic).
-func UnpackLayer(root string, layer io.Reader) error {
-	te := newTarExtractor()
+func UnpackLayer(root string, layer io.Reader, opt *MapOptions) error {
+	var mapOptions MapOptions
+	if opt != nil {
+		mapOptions = *opt
+	}
+	te := newTarExtractor(mapOptions)
 	tr := tar.NewReader(layer)
 	for {
 		hdr, err := tr.Next()
@@ -74,7 +79,7 @@ func isLayerType(mediaType string) bool {
 // extraction.
 //
 // FIXME: This interface is ugly.
-func UnpackManifest(ctx context.Context, engine cas.Engine, bundle string, manifest v1.Manifest) error {
+func UnpackManifest(ctx context.Context, engine cas.Engine, bundle string, manifest v1.Manifest, opt *MapOptions) error {
 	// Create the bundle directory. We only error out if config.json or rootfs/
 	// already exists, because we cannot be sure that the user intended us to
 	// extract over an existing bundle.
@@ -101,6 +106,19 @@ func UnpackManifest(ctx context.Context, engine cas.Engine, bundle string, manif
 
 	if err := os.Mkdir(rootfsPath, 0755); err != nil {
 		return fmt.Errorf("unpack manifest: creating rootfs: %s", err)
+	}
+
+	// Make sure that the owner is correct.
+	rootUID, err := idtools.ToHost(0, opt.UIDMappings)
+	if err != nil {
+		return fmt.Errorf("unpack manifest: creating rootfs: tohost(uid): %s", err)
+	}
+	rootGID, err := idtools.ToHost(0, opt.GIDMappings)
+	if err != nil {
+		return fmt.Errorf("unpack manifest: creating rootfs: tohost(gid): %s", err)
+	}
+	if err := os.Lchown(rootfsPath, rootUID, rootGID); err != nil {
+		return fmt.Errorf("unpack manifest: creating rootfs: chown: %s", err)
 	}
 
 	// Currently, many different images in the wild don't specify what the
@@ -158,7 +176,7 @@ func UnpackManifest(ctx context.Context, engine cas.Engine, bundle string, manif
 		layerHash := sha256.New()
 		layer := io.TeeReader(layerRaw, layerHash)
 
-		if err := UnpackLayer(rootfsPath, layer); err != nil {
+		if err := UnpackLayer(rootfsPath, layer, opt); err != nil {
 			return fmt.Errorf("unpack manifest: layer %s: %s", layerBlob.Digest, err)
 		}
 		layerGzip.Close()
@@ -170,8 +188,6 @@ func UnpackManifest(ctx context.Context, engine cas.Engine, bundle string, manif
 	}
 
 	// Generate a runtime configuration file from v1.Image.
-	// FIXME: This currently doesn't support numeric user IDs, because it
-	//        doesn't know rootfsPath. We can fix this now, though.
 	logrus.WithFields(logrus.Fields{
 		"config": manifest.Config.Digest,
 	}).Infof("unpack manifest: unpacking config")

cmd/umoci/repack.go
@@ -30,6 +30,7 @@ import (
 	"github.com/cyphar/umoci/image/cas"
 	"github.com/cyphar/umoci/image/generator"
 	"github.com/cyphar/umoci/image/layer"
+	"github.com/cyphar/umoci/pkg/idtools"
 	"github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/urfave/cli"
 	"github.com/vbatts/go-mtree"
@@ -68,6 +69,14 @@ manifest and configuration information uses the new diff atop the old manifest.`
 			Name:  "tag",
 			Usage: "tag name for repacked image",
 		},
+		cli.StringSliceFlag{
+			Name:  "uid-map",
+			Usage: "specifies a uid mapping to use when repacking",
+		},
+		cli.StringSliceFlag{
+			Name:  "gid-map",
+			Usage: "specifies a gid mapping to use when repacking",
+		},
 	},
 
 	Action: repack,
@@ -87,6 +96,27 @@ func repack(ctx *cli.Context) error {
 	if fromName == "" {
 		return fmt.Errorf("reference name cannot be empty")
 	}
+
+	// Parse map options.
+	mapOptions := layer.MapOptions{}
+	for _, uidmap := range ctx.StringSlice("uid-map") {
+		idMap, err := idtools.ParseMapping(uidmap)
+		if err != nil {
+			return fmt.Errorf("failure parsing --uid-map %s: %s", uidmap, err)
+		}
+		mapOptions.UIDMappings = append(mapOptions.UIDMappings, idMap)
+	}
+	for _, gidmap := range ctx.StringSlice("gid-map") {
+		idMap, err := idtools.ParseMapping(gidmap)
+		if err != nil {
+			return fmt.Errorf("failure parsing --gid-map %s: %s", gidmap, err)
+		}
+		mapOptions.GIDMappings = append(mapOptions.GIDMappings, idMap)
+	}
+	logrus.WithFields(logrus.Fields{
+		"map.uid": mapOptions.UIDMappings,
+		"map.gid": mapOptions.GIDMappings,
+	}).Infof("parsed mappings")
 
 	// Get a reference to the CAS.
 	engine, err := cas.Open(imagePath)
@@ -143,7 +173,7 @@ func repack(ctx *cli.Context) error {
 		"ndiff": len(diffs),
 	}).Debugf("umoci: checked mtree spec")
 
-	reader, err := layer.GenerateLayer(fullRootfsPath, diffs)
+	reader, err := layer.GenerateLayer(fullRootfsPath, diffs, &mapOptions)
 	if err != nil {
 		return err
 	}
@@ -169,6 +199,7 @@ func repack(ctx *cli.Context) error {
 	go func() {
 		_, err := io.Copy(gzw, hashReader)
 		if err != nil {
+			logrus.Warnf("failed when copying to gzip: %s", err)
 			pipeWriter.CloseWithError(err)
 			return
 		}

image/layer/generate.go
@@ -44,7 +44,12 @@ func (ids inodeDeltas) Swap(i, j int)      { ids[i], ids[j] = ids[j], ids[i] }
 // provided path (which should be the rootfs of the layer that was diffed). The
 // returned reader is for the *raw* tar data, it is the caller's responsibility
 // to gzip it.
-func GenerateLayer(path string, deltas []mtree.InodeDelta) (io.ReadCloser, error) {
+func GenerateLayer(path string, deltas []mtree.InodeDelta, opt *MapOptions) (io.ReadCloser, error) {
+	var mapOptions MapOptions
+	if opt != nil {
+		mapOptions = *opt
+	}
+
 	reader, writer := io.Pipe()
 
 	go func() {
@@ -53,7 +58,7 @@ func GenerateLayer(path string, deltas []mtree.InodeDelta) (io.ReadCloser, error
 		// We can't just dump all of the file contents into a tar file. We need
 		// to emulate a proper tar generator. Luckily there aren't that many
 		// things to emulate (and we can do them all in tar.go).
-		tg := newTarGenerator(writer)
+		tg := newTarGenerator(writer, mapOptions)
 		defer tg.tw.Close()
 
 		// Sort the delta paths.
@@ -73,7 +78,7 @@ func GenerateLayer(path string, deltas []mtree.InodeDelta) (io.ReadCloser, error
 			switch delta.Type() {
 			case mtree.Modified, mtree.Extra:
 				if err := tg.AddFile(name, fullPath); err != nil {
-					logrus.Warnf("could not add file: %s", err)
+					logrus.Warnf("could not add file %s: %s", name, err)
 					writer.CloseWithError(err)
 					return
 				}

image/layer/tar_extract.go
@@ -33,17 +33,21 @@ import (
 
 // FIXME: Add tarExtractor.
 type tarExtractor struct {
+	// mapOptions is the set of mapping options to use when extracting filesystem layers.
+	mapOptions MapOptions
 }
 
 // newTarExtractor creates a new tarExtractor.
-func newTarExtractor() *tarExtractor {
-	return &tarExtractor{}
+func newTarExtractor(opt MapOptions) *tarExtractor {
+	return &tarExtractor{
+		mapOptions: opt,
+	}
 }
 
-// applyMetadata applies the state described in tar.Header to the filesystem at
-// the given path. No sanity checking is done of the tar.Header's pathname or
-// other information.
-func (te *tarExtractor) applyMetadata(path string, hdr *tar.Header) error {
+// restoreMetadata applies the state described in tar.Header to the filesystem
+// at the given path. No sanity checking is done of the tar.Header's pathname
+// or other information. In addition, no mapping is done of the header.
+func restoreMetadata(path string, hdr *tar.Header) error {
 	// Some of the tar.Header fields don't match the OS API.
 	fi := hdr.FileInfo()
 
@@ -86,7 +90,10 @@ func (te *tarExtractor) applyMetadata(path string, hdr *tar.Header) error {
 	// Apply xattrs. In order to make sure that we *only* have the xattr set we
 	// want, we first clear the set of xattrs from the file then apply the ones
 	// set in the tar.Header.
-	// FIXME: This will almost certainly break horribly on RedHat.
+	// XXX: This will almost certainly break horribly on RedHat.
+	// FIXME: We really should not be clearing xattrs if the hdr has no Xattrs
+	//        entries (or something like that). Otherwise we're messing with
+	//        xattrs in rootless images.
 	if err := system.Lclearxattrs(path); err != nil {
 		return fmt.Errorf("apply metadata: %s: %s", path, err)
 	}
@@ -102,6 +109,21 @@ func (te *tarExtractor) applyMetadata(path string, hdr *tar.Header) error {
 
 	// TODO.
 	return nil
+}
+
+// applyMetadata applies the state described in tar.Header to the filesystem at
+// the given path, using the state of the tarExtractor to remap information
+// within the header. This should only be used with headers from a tar layer
+// (not from the filesystem). No sanity checking is done of the tar.Header's
+// pathname or other information.
+func (te *tarExtractor) applyMetadata(path string, hdr *tar.Header) error {
+	// Modify the header.
+	if err := unmapHeader(hdr, te.mapOptions); err != nil {
+		return err
+	}
+
+	// Restore it on the filesystme.
+	return restoreMetadata(path, hdr)
 }
 
 // unpackEntry extracts the given tar.Header to the provided root, ensuring
@@ -149,11 +171,19 @@ func (te *tarExtractor) unpackEntry(root string, hdr *tar.Header, r io.Reader) (
 			return err
 		}
 
+		// More faking to trick restoreMetadata to actually restore the directory.
+		dirHdr.Typeflag = tar.TypeDir
+		dirHdr.Linkname = ""
+
 		// Ensure that after everything we correctly re-apply the old metadata.
+		// We don't map this header because we're restoring files that already
+		// existed on the filesystem, not from a tar layer.
 		defer func() {
 			// Only overwrite the error if there wasn't one already.
-			if err := te.applyMetadata(dir, dirHdr); err != nil && Err == nil {
-				Err = err
+			if err := restoreMetadata(dir, dirHdr); err != nil {
+				if Err == nil {
+					Err = err
+				}
 			}
 		}()
 	}

image/layer/tar_extract_test.go
@@ -62,7 +62,7 @@ func testUnpackEntrySanitiseHelper(t *testing.T, dir, file, prefix string) func(
 			ChangeTime: time.Now(),
 		}
 
-		te := newTarExtractor()
+		te := newTarExtractor(MapOptions{})
 		if err := te.unpackEntry(rootfs, hdr, bytes.NewBuffer(ctrValue)); err != nil {
 			t.Fatalf("unexpected unpackEntry error: %s", err)
 		}
@@ -185,7 +185,7 @@ func TestUnpackEntryParentDir(t *testing.T) {
 		ChangeTime: time.Now(),
 	}
 
-	te := newTarExtractor()
+	te := newTarExtractor(MapOptions{})
 	if err := te.unpackEntry(rootfs, hdr, bytes.NewBuffer(ctrValue)); err != nil {
 		t.Fatalf("unexpected unpackEntry error: %s", err)
 	}
@@ -272,7 +272,7 @@ func TestUnpackEntryWhiteout(t *testing.T) {
 				Typeflag: tar.TypeReg,
 			}
 
-			te := newTarExtractor()
+			te := newTarExtractor(MapOptions{})
 			if err := te.unpackEntry(dir, hdr, nil); err != nil {
 				t.Fatalf("unexpected error in unpackEntry: %s", err)
 			}
@@ -325,7 +325,7 @@ func TestUnpackHardlink(t *testing.T) {
 		hardFileB = "hard link to symlink"
 	)
 
-	te := newTarExtractor()
+	te := newTarExtractor(MapOptions{})
 
 	// Regular file.
 	hdr = &tar.Header{

image/layer/tar_generate.go
@@ -32,6 +32,10 @@ import (
 type tarGenerator struct {
 	tw *tar.Writer
 
+	// mapOptions is the set of mapping options for modifying entries before
+	// they're added to the layer.
+	mapOptions MapOptions
+
 	// Hardlink mapping.
 	// XXX: Do we need to handle having a rootfs/ which is on more than one
 	//      filesystem? In which case this will have to be more complicated
@@ -52,9 +56,10 @@ type tarGenerator struct {
 
 // newTarGenerator creates a new tarGenerator using the provided writer as the
 // output writer.
-func newTarGenerator(w io.Writer) *tarGenerator {
+func newTarGenerator(w io.Writer, opt MapOptions) *tarGenerator {
 	return &tarGenerator{
 		tw:          tar.NewWriter(w),
+		mapOptions:  opt,
 		inodes:      map[uint64]string{},
 		directories: map[string]bool{},
 	}
@@ -150,6 +155,10 @@ func (tg *tarGenerator) AddFile(name, path string) error {
 
 	// XXX: What about xattrs.
 
+	// Apply any header mappings.
+	if err := mapHeader(hdr, tg.mapOptions); err != nil {
+		return err
+	}
 	if err := tg.tw.WriteHeader(hdr); err != nil {
 		return err
 	}

commit d829321ce6a8cdc19b8f5785f9f11a4e0ebe5b94

repack: change --[ug]id-map= UX to match user_namespaces(7)
Our IDMap interfaces were not matching what a user who is familiar with
user_namespaces(7) would expect. This caused me some confusion while
trying to debug a user namespace bug. While this is a breaking change,
I'm fairly confident that there are almost no users of this feature.

In addition, actually add documentation on the --[ug]id-map format
(which was previously not explicitly documented at all).

Fixes: 9f88dd9a114d ("{un,re}pack: implement ID mapping")
Signed-off-by: Aleksa Sarai <asarai@suse.de>

cmd/umoci/unpack.go
@@ -55,11 +55,11 @@ creation with umoci-repack(1).`,
 	Flags: []cli.Flag{
 		cli.StringSliceFlag{
 			Name:  "uid-map",
-			Usage: "specifies a uid mapping to use when repacking",
+			Usage: "specifies a uid mapping to use when repacking (container:host:size)",
 		},
 		cli.StringSliceFlag{
 			Name:  "gid-map",
-			Usage: "specifies a gid mapping to use when repacking",
+			Usage: "specifies a gid mapping to use when repacking (container:host:size)",
 		},
 		cli.BoolFlag{
 			Name:  "rootless",
@@ -94,10 +94,10 @@ func unpack(ctx *cli.Context) error {
 	meta.MapOptions.Rootless = ctx.Bool("rootless")
 	if meta.MapOptions.Rootless {
 		if !ctx.IsSet("uid-map") {
-			ctx.Set("uid-map", fmt.Sprintf("%d:0:1", os.Geteuid()))
+			ctx.Set("uid-map", fmt.Sprintf("0:%d:1", os.Geteuid()))
 		}
 		if !ctx.IsSet("gid-map") {
-			ctx.Set("gid-map", fmt.Sprintf("%d:0:1", os.Getegid()))
+			ctx.Set("gid-map", fmt.Sprintf("0:%d:1", os.Getegid()))
 		}
 	}
 	// Parse and set up the mapping options.

pkg/idtools/idtools.go
@@ -60,7 +60,7 @@ func ToContainer(hostID int, idMap []rspec.LinuxIDMapping) (int, error) {
 	return -1, errors.Errorf("host id %d cannot be mapped to a container id", hostID)
 }
 
-// ParseMapping takes a mapping string of the form "host:container[:size]" and
+// ParseMapping takes a mapping string of the form "container:host[:size]" and
 // returns the corresponding rspec.LinuxIDMapping. An error is returned if not
 // enough fields are provided or are otherwise invalid. The default size is 1.
 func ParseMapping(spec string) (rspec.LinuxIDMapping, error) {
@@ -80,14 +80,14 @@ func ParseMapping(spec string) (rspec.LinuxIDMapping, error) {
 		return rspec.LinuxIDMapping{}, errors.Errorf("invalid number of fields in mapping '%s': %d", spec, len(parts))
 	}
 
-	hostID, err = strconv.Atoi(parts[0])
-	if err != nil {
-		return rspec.LinuxIDMapping{}, errors.Wrap(err, "invalid hostID in mapping")
-	}
-
-	contID, err = strconv.Atoi(parts[1])
+	contID, err = strconv.Atoi(parts[0])
 	if err != nil {
 		return rspec.LinuxIDMapping{}, errors.Wrap(err, "invalid containerID in mapping")
+	}
+
+	hostID, err = strconv.Atoi(parts[1])
+	if err != nil {
+		return rspec.LinuxIDMapping{}, errors.Wrap(err, "invalid hostID in mapping")
 	}
 
 	return rspec.LinuxIDMapping{

pkg/idtools/idtools_test.go
@@ -297,10 +297,10 @@ func TestParseIDMapping(t *testing.T) {
 		failure               bool
 	}{
 		{spec: "0:0:1", host: 0, container: 0, size: 1, failure: false},
-		{spec: "100:32:2421", host: 100, container: 32, size: 2421, failure: false},
-		{spec: "1337:0:1924", host: 1337, container: 0, size: 1924, failure: false},
-		{spec: "1:2", host: 1, container: 2, size: 1, failure: false},
-		{spec: "123:422", host: 123, container: 422, size: 1, failure: false},
+		{spec: "32:100:2421", host: 100, container: 32, size: 2421, failure: false},
+		{spec: "0:1337:1924", host: 1337, container: 0, size: 1924, failure: false},
+		{spec: "2:1", host: 1, container: 2, size: 1, failure: false},
+		{spec: "422:123", host: 123, container: 422, size: 1, failure: false},
 		{spec: "", host: 0, container: 0, size: 0, failure: true},
 		{spec: "::", host: 0, container: 0, size: 0, failure: true},
 		{spec: "1:2:", host: 0, container: 0, size: 0, failure: true},
@@ -318,13 +318,13 @@ func TestParseIDMapping(t *testing.T) {
 				t.Errorf("unexpected error: %+v", err)
 			} else {
 				if idMap.HostID != test.host {
-					t.Errorf("expected to get host %d, got %d", test.host, idMap.HostID)
+					t.Errorf("%q: expected to get host %d, got %d", test.spec, test.host, idMap.HostID)
 				}
 				if idMap.ContainerID != test.container {
-					t.Errorf("expected to get container %d, got %d", test.container, idMap.HostID)
+					t.Errorf("%q: expected to get container %d, got %d", test.spec, test.container, idMap.HostID)
 				}
 				if idMap.Size != test.size {
-					t.Errorf("expected to get size %d, got %d", test.size, idMap.HostID)
+					t.Errorf("%q: expected to get size %d, got %d", test.spec, test.size, idMap.HostID)
 				}
 			}
 		}

test/pack_mapping.bats
@@ -35,7 +35,7 @@ function teardown() {
 	BUNDLE_B="$(setup_tmpdir)"
 
 	# Unpack the image.
-	umoci unpack --image "${IMAGE}:${TAG}" --uid-map "1337:0:65535" --gid-map "8888:0:65535" "$BUNDLE_A"
+	umoci unpack --image "${IMAGE}:${TAG}" --uid-map "0:1337:65535" --gid-map "0:8888:65535" "$BUNDLE_A"
 	[ "$status" -eq 0 ]
 	bundle-verify "$BUNDLE_A"
 
@@ -53,7 +53,7 @@ function teardown() {
 	}'
 
 	# Unpack the image with a differen uid and gid mapping.
-	umoci unpack --image "${IMAGE}:${TAG}" --uid-map "8080:0:65535" --gid-map "7777:0:65535" "$BUNDLE_B"
+	umoci unpack --image "${IMAGE}:${TAG}" --uid-map "0:8080:65535" --gid-map "0:7777:65535" "$BUNDLE_B"
 	[ "$status" -eq 0 ]
 	bundle-verify "$BUNDLE_B"
 
@@ -73,7 +73,7 @@ function teardown() {
 	image-verify "${IMAGE}"
 }
 
-@test "umoci repack [with unpack --uid-map --gid-map]" {
+@test "umoci repack [--uid-map --gid-map]" {
 	# We do a bunch of remapping tricks, which we can't really do if we're not root.
 	requires root
 
@@ -84,7 +84,7 @@ function teardown() {
 	BUNDLE_C="$(setup_tmpdir)"
 
 	# Unpack the image.
-	umoci unpack --image "${IMAGE}:${TAG}" --uid-map "1337:0:65535" --gid-map "7331:0:65535" "$BUNDLE_A"
+	umoci unpack --image "${IMAGE}:${TAG}" --uid-map "0:1337:65535" --gid-map "0:7331:65535" "$BUNDLE_A"
 	[ "$status" -eq 0 ]
 	bundle-verify "$BUNDLE_A"
 
@@ -101,7 +101,7 @@ function teardown() {
 	image-verify "${IMAGE}"
 
 	# Unpack it again with a different mapping.
-	umoci unpack --image "${IMAGE}:${TAG}-new" --uid-map "4000:0:65535" --gid-map "4000:0:65535" "$BUNDLE_B"
+	umoci unpack --image "${IMAGE}:${TAG}-new" --uid-map "0:4000:65535" --gid-map "0:4000:65535" "$BUNDLE_B"
 	[ "$status" -eq 0 ]
 	bundle-verify "$BUNDLE_B"
 