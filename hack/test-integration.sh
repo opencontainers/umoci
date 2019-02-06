b2192204e06d9e06b00c9c21e8c9ab3ee7d6cf07
Aleksa Sarai <asarai@suse.de>
2019-02-07 03:22:54 +1100

    *: harden shell scripts
    I've since learned about "set -Eeuo pipefail" so use it everywhere.
    
    Signed-off-by: Aleksa Sarai <asarai@suse.de>

--- hack/test-integration.sh
+++ hack/test-integration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # umoci: Umoci Modifies Open Containers' Images
-# Copyright (C) 2016, 2017, 2018 SUSE LLC.
+# Copyright (C) 2016-2019 SUSE LLC.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -ex
+set -Eeuxo pipefail
 
 export COVER="${COVER:-0}"
 
