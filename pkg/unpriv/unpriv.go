01954f533cb4e8cb9a4adf2ab342bb7ec4c5d816
Aleksa Sarai <asarai@suse.de>
2019-09-12 22:43:17 +1000

    *: add copyright statement
    Signed-off-by: Aleksa Sarai <asarai@suse.de>

--- pkg/unpriv/unpriv.go
+++ pkg/unpriv/unpriv.go
@@ -1,6 +1,6 @@
 /*
  * umoci: Umoci Modifies Open Containers' Images
- * Copyright (C) 2016, 2017, 2018 SUSE LLC.
+ * Copyright (C) 2016-2019 SUSE LLC.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
