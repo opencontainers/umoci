opencontainers/umoci

commit 6fbd32e48b66b5a9598799360d10637517d1c903

Make Makefile more portable
Signed-off-by: Mateusz Kwiatkowski <mateusz@serveraptor.com>

Makefile
@@ -14,11 +14,13 @@
 # limitations under the License.
 
 # Use bash, so that we can do process substitution.
-SHELL = /bin/bash
+SHELL := $(shell which bash)
 
 # Go tools.
 GO ?= go
 GO_MD2MAN ?= go-md2man
+GOOS ?= $(shell go env GOOS)
+GOARCH ?= $(shell go env GOARCH)
 export GO111MODULE=on
 
 # Set up the ... lovely ... GOPATH hacks.
@@ -68,13 +70,20 @@ BASE_FLAGS := ${BUILD_FLAGS} -tags "${BUILDTAGS}"
 BASE_LDFLAGS := -s -w -X ${PROJECT}.gitCommit=${COMMIT} -X ${PROJECT}.version=${VERSION}
 
 # Specific build flags for build type.
-DYN_BUILD_FLAGS := ${BASE_FLAGS} -buildmode=pie -ldflags "${BASE_LDFLAGS}"
-TEST_BUILD_FLAGS := ${BASE_FLAGS} -buildmode=pie -ldflags "${BASE_LDFLAGS} -X ${PROJECT}/pkg/testutils.binaryType=test"
+ifeq ($(GOOS), linux)
+	TEST_BUILD_FLAGS := ${BASE_FLAGS} -buildmode=pie -ldflags "${BASE_LDFLAGS} -X ${PROJECT}/pkg/testutils.binaryType=test"		   DYN_BUILD_FLAGS := ${BASE_FLAGS} -buildmode=pie -ldflags "${BASE_LDFLAGS}"
+	TEST_BUILD_FLAGS := ${BASE_FLAGS} -buildmode=pie -ldflags "${BASE_LDFLAGS} -X ${PROJECT}/pkg/testutils.binaryType=test"
+else
+	DYN_BUILD_FLAGS := ${BASE_FLAGS} -ldflags "${BASE_LDFLAGS}"
+	TEST_BUILD_FLAGS := ${BASE_FLAGS} -ldflags "${BASE_LDFLAGS} -X ${PROJECT}/pkg/testutils.binaryType=test"
+endif
+
+
 STATIC_BUILD_FLAGS := ${BASE_FLAGS} -ldflags "${BASE_LDFLAGS} -extldflags '-static'"
 
 # Installation directories.
 DESTDIR ?=
-PREFIX ?=/usr
+PREFIX ?=/usr/local
 BINDIR ?=$(PREFIX)/bin
 MANDIR ?=$(PREFIX)/share/man
 
@@ -85,7 +94,7 @@ GO_SRC = $(shell find . -type f -name '*.go')
 # NOTE: If you change these make sure you also update local-validate-build.
 
 umoci: $(GO_SRC)
-	$(GO) build ${DYN_BUILD_FLAGS} -o $(BUILD_DIR)/$@ ${CMD}
+	GOOS=$(GOOS) GOARCH=$(GOARCH) $(GO) build ${DYN_BUILD_FLAGS} -o $(BUILD_DIR)/$@ ${CMD}
 
 umoci.static: $(GO_SRC)
 	env CGO_ENABLED=0 $(GO) build ${STATIC_BUILD_FLAGS} -o $(BUILD_DIR)/$@ ${CMD}

commit a5d1ee156b76c4f823ae2554a0fd2e2ab9aaccaa

makefile: fix bad build flags
Fix mistake in the Makefile which prevents the version field (as well as
some other build flags) from being passed to "go build". Update the
"runc --version" integration test to make sure that the contents of
VERSION are actually present.

Fixes: 6fbd32e48b66 ("Make Makefile more portable")
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

Makefile
@@ -71,7 +71,7 @@ BASE_LDFLAGS := -s -w -X ${PROJECT}.gitCommit=${COMMIT} -X ${PROJECT}.version=${
 
 # Specific build flags for build type.
 ifeq ($(GOOS), linux)
-	TEST_BUILD_FLAGS := ${BASE_FLAGS} -buildmode=pie -ldflags "${BASE_LDFLAGS} -X ${PROJECT}/pkg/testutils.binaryType=test"		   DYN_BUILD_FLAGS := ${BASE_FLAGS} -buildmode=pie -ldflags "${BASE_LDFLAGS}"
+	DYN_BUILD_FLAGS := ${BASE_FLAGS} -buildmode=pie -ldflags "${BASE_LDFLAGS}"
 	TEST_BUILD_FLAGS := ${BASE_FLAGS} -buildmode=pie -ldflags "${BASE_LDFLAGS} -X ${PROJECT}/pkg/testutils.binaryType=test"
 else
 	DYN_BUILD_FLAGS := ${BASE_FLAGS} -ldflags "${BASE_LDFLAGS}"

test/help.bats
@@ -17,13 +17,15 @@
 load helpers
 
 @test "umoci --version" {
+	VERSION="$(cat "$INTEGRATION_ROOT/../VERSION")"
+
 	umoci --version
 	[ "$status" -eq 0 ]
-	[[ "$output" =~ "umoci version "+ ]]
+	[[ "$output" =~ "umoci version $VERSION"+ ]]
 
 	umoci -v
 	[ "$status" -eq 0 ]
-	[[ "$output" =~ "umoci version "+ ]]
+	[[ "$output" =~ "umoci version $VERSION"+ ]]
 }
 
 @test "umoci --help" {