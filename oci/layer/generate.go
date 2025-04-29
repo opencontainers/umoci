opencontainers/umoci

commit a425f92e910345405e8c8d8bb67e7da510ac1b65

layer: add support for translating overlay whiteouts to OCI ones
This is the compliment to the previous commit, in case people want to
re-pack layers using the OCI standard instead of the overlayfs standard.

Also add a PackOptions{} struct, similar to UnpackOptions{}, which
can control the low level behavior of the packing routines. I expect we'll
use this later for hooks, too.

Signed-off-by: Tycho Andersen <tycho@tycho.ws>

oci/layer/generate.go
@@ -43,10 +43,10 @@ func (ids inodeDeltas) Swap(i, j int)      { ids[i], ids[j] = ids[j], ids[i] }
 // provided path (which should be the rootfs of the layer that was diffed). The
 // returned reader is for the *raw* tar data, it is the caller's responsibility
 // to gzip it.
-func GenerateLayer(path string, deltas []mtree.InodeDelta, opt *MapOptions) (io.ReadCloser, error) {
-	var mapOptions MapOptions
+func GenerateLayer(path string, deltas []mtree.InodeDelta, opt *RepackOptions) (io.ReadCloser, error) {
+	var packOptions RepackOptions
 	if opt != nil {
-		mapOptions = *opt
+		packOptions = *opt
 	}
 
 	reader, writer := io.Pipe()
@@ -61,7 +61,7 @@ func GenerateLayer(path string, deltas []mtree.InodeDelta, opt *MapOptions) (io.
 		// We can't just dump all of the file contents into a tar file. We need
 		// to emulate a proper tar generator. Luckily there aren't that many
 		// things to emulate (and we can do them all in tar.go).
-		tg := newTarGenerator(writer, mapOptions)
+		tg := newTarGenerator(writer, packOptions.MapOptions)
 
 		// Sort the delta paths.
 		// FIXME: We need to add whiteouts first, otherwise we might end up
@@ -79,6 +79,19 @@ func GenerateLayer(path string, deltas []mtree.InodeDelta, opt *MapOptions) (io.
 
 			switch delta.Type() {
 			case mtree.Modified, mtree.Extra:
+				if packOptions.TranslateOverlayWhiteouts {
+					fi, err := os.Stat(fullPath)
+					if err != nil {
+						return errors.Wrapf(err, "couldn't determine overlay whiteout for %s", fullPath)
+					}
+
+					if isOverlayWhiteout(fi) {
+						if err := tg.AddWhiteout(fullPath); err != nil {
+							return errors.Wrap(err, "generate whiteout from overlayfs")
+						}
+					}
+					continue
+				}
 				if err := tg.AddFile(name, fullPath); err != nil {
 					log.Warnf("generate layer: could not add file '%s': %s", name, err)
 					return errors.Wrap(err, "generate layer file")
@@ -105,12 +118,12 @@ func GenerateLayer(path string, deltas []mtree.InodeDelta, opt *MapOptions) (io.
 // GenerateInsertLayer generates a completely new layer from "root"to be
 // inserted into the image at "target". If "root" is an empty string then the
 // "target" will be removed via a whiteout.
-func GenerateInsertLayer(root string, target string, opaque bool, opt *MapOptions) io.ReadCloser {
+func GenerateInsertLayer(root string, target string, opaque bool, opt *RepackOptions) io.ReadCloser {
 	root = CleanPath(root)
 
-	var mapOptions MapOptions
+	var packOptions RepackOptions
 	if opt != nil {
-		mapOptions = *opt
+		packOptions = *opt
 	}
 
 	reader, writer := io.Pipe()
@@ -121,7 +134,7 @@ func GenerateInsertLayer(root string, target string, opaque bool, opt *MapOption
 			_ = writer.CloseWithError(errors.Wrap(Err, "generate layer"))
 		}()
 
-		tg := newTarGenerator(writer, mapOptions)
+		tg := newTarGenerator(writer, packOptions.MapOptions)
 
 		if opaque {
 			if err := tg.AddOpaqueWhiteout(target); err != nil {
@@ -137,6 +150,11 @@ func GenerateInsertLayer(root string, target string, opaque bool, opt *MapOption
 			}
 
 			pathInTar := path.Join(target, curPath[len(root):])
+			if packOptions.TranslateOverlayWhiteouts && isOverlayWhiteout(info) {
+				log.Debugf("converting overlayfs whiteout %s to OCI whiteout", pathInTar)
+				return tg.AddWhiteout(pathInTar)
+			}
+
 			return tg.AddFile(pathInTar, curPath)
 		})
 	}()

oci/layer/generate_linux_test.go
@@ -1,0 +1,100 @@
+// +build linux
+
+package layer
+
+import (
+	"archive/tar"
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/opencontainers/umoci/pkg/fseval"
+	"github.com/stretchr/testify/assert"
+	"github.com/vbatts/go-mtree"
+	"golang.org/x/sys/unix"
+)
+
+func TestInsertLayerTranslateOverlayWhiteouts(t *testing.T) {
+	assert := assert.New(t)
+	dir, err := ioutil.TempDir("", "umoci-TestTranslateOverlayWhiteouts")
+	assert.NoError(err)
+	defer os.RemoveAll(dir)
+
+	mknodOk, err := canMknod(dir)
+	if err != nil {
+		t.Fatalf("couldn't mknod in dir: %v", err)
+	}
+
+	if !mknodOk {
+		t.Skip("skipping overlayfs test on kernel < 5.8")
+	}
+
+	testNode := path.Join(dir, "test")
+	err = unix.Mknod(testNode, unix.S_IFCHR|0666, int(unix.Mkdev(0, 0)))
+	assert.NoError(err)
+
+	packOptions := RepackOptions{TranslateOverlayWhiteouts: true}
+	reader := GenerateInsertLayer(dir, "/", false, &packOptions)
+	defer reader.Close()
+
+	tr := tar.NewReader(reader)
+	hdr, err := tr.Next()
+	assert.NoError(err)
+	assert.Equal(hdr.Name, "/")
+
+	hdr, err = tr.Next()
+	assert.NoError(err)
+
+	assert.Equal(int32(hdr.Typeflag), int32(tar.TypeReg))
+	assert.Equal(hdr.Name, whPrefix+"test")
+	_, err = tr.Next()
+	assert.Equal(err, io.EOF)
+}
+
+func TestGenerateLayerTranslateOverlayWhiteouts(t *testing.T) {
+	assert := assert.New(t)
+	dir, err := ioutil.TempDir("", "umoci-TestTranslateOverlayWhiteouts")
+	assert.NoError(err)
+	defer os.RemoveAll(dir)
+
+	mknodOk, err := canMknod(dir)
+	if err != nil {
+		t.Fatalf("couldn't mknod in dir: %v", err)
+	}
+
+	if !mknodOk {
+		t.Skip("skipping overlayfs test on kernel < 5.8")
+	}
+
+	testNode := path.Join(dir, "test")
+	err = unix.Mknod(testNode, unix.S_IFCHR|0666, int(unix.Mkdev(0, 0)))
+	assert.NoError(err)
+
+	packOptions := RepackOptions{TranslateOverlayWhiteouts: true}
+	// something reasonable
+	mtreeKeywords := []mtree.Keyword{
+		"size",
+		"type",
+		"uid",
+		"gid",
+		"mode",
+	}
+	deltas, err := mtree.Check(dir, nil, mtreeKeywords, fseval.Default)
+	assert.NoError(err)
+
+	reader, err := GenerateLayer(dir, deltas, &packOptions)
+	assert.NoError(err)
+	defer reader.Close()
+
+	tr := tar.NewReader(reader)
+
+	hdr, err := tr.Next()
+	assert.NoError(err)
+
+	assert.Equal(int32(hdr.Typeflag), int32(tar.TypeReg))
+	assert.Equal(path.Base(hdr.Name), whPrefix+"test")
+	_, err = tr.Next()
+	assert.Equal(err, io.EOF)
+}

cmd/umoci/insert.go
@@ -156,7 +156,8 @@ func insert(ctx *cli.Context) error {
 		return err
 	}
 
-	reader := layer.GenerateInsertLayer(sourcePath, targetPath, ctx.IsSet("opaque"), &meta.MapOptions)
+	packOptions := layer.RepackOptions{MapOptions: meta.MapOptions}
+	reader := layer.GenerateInsertLayer(sourcePath, targetPath, ctx.IsSet("opaque"), &packOptions)
 	defer reader.Close()
 
 	var history *ispec.History

go.mod
@@ -22,6 +22,7 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/rootless-containers/proto v0.1.0
 	github.com/sirupsen/logrus v1.6.0 // indirect
+	github.com/stretchr/testify v1.6.1
 	github.com/tj/assert v0.0.3 // indirect
 	github.com/urfave/cli v1.22.4
 	github.com/vbatts/go-mtree v0.5.0

oci/layer/generate_test.go
@@ -74,7 +74,7 @@ func TestGenerate(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	reader, err := GenerateLayer(dir, diffs, &MapOptions{})
+	reader, err := GenerateLayer(dir, diffs, &RepackOptions{})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -188,7 +188,7 @@ func TestGenerateMissingFileError(t *testing.T) {
 	}
 
 	// Generate a layer where the changed file is missing after the diff.
-	reader, err := GenerateLayer(dir, diffs, &MapOptions{})
+	reader, err := GenerateLayer(dir, diffs, &RepackOptions{})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -253,7 +253,7 @@ func TestGenerateWrongRootError(t *testing.T) {
 	}
 
 	// Generate a layer with the wrong root directory.
-	reader, err := GenerateLayer(filepath.Join(dir, "some"), diffs, &MapOptions{})
+	reader, err := GenerateLayer(filepath.Join(dir, "some"), diffs, &RepackOptions{})
 	if err != nil {
 		t.Fatal(err)
 	}

oci/layer/tar_extract_linux_test.go
@@ -24,11 +24,23 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"syscall"
 	"testing"
 
 	"golang.org/x/sys/unix"
 )
+
+func canMknod(dir string) (bool, error) {
+	testNode := filepath.Join(dir, "test")
+	err := unix.Mknod(testNode, unix.S_IFCHR|0666, int(unix.Mkdev(0, 0)))
+	if err != nil {
+		if os.IsPermission(err) {
+			return false, nil
+		}
+
+		return false, err
+	}
+	return true, os.Remove(testNode)
+}
 
 func TestUnpackEntryOverlayFSWhiteout(t *testing.T) {
 	dir, err := ioutil.TempDir("", "umoci-TestOverlayFSWhiteout")
@@ -37,13 +49,13 @@ func TestUnpackEntryOverlayFSWhiteout(t *testing.T) {
 	}
 	defer os.RemoveAll(dir)
 
-	err = unix.Mknod(filepath.Join(dir, "test"), syscall.S_IFCHR|0666, int(unix.Mkdev(0, 0)))
+	mknodOk, err := canMknod(dir)
 	if err != nil {
-		if os.IsPermission(err) {
-			t.Skip("skipping overlayfs test on kernel < 5.8")
-		}
+		t.Fatalf("couldn't mknod in dir: %v", err)
+	}
 
-		t.Fatalf("couldn't mknod: %v", err)
+	if !mknodOk {
+		t.Skip("skipping overlayfs test on kernel < 5.8")
 	}
 
 	headers := []pseudoHdr{
@@ -82,9 +94,8 @@ func TestUnpackEntryOverlayFSWhiteout(t *testing.T) {
 		t.Fatalf("failed to stat `file`: %v", err)
 	}
 
-	sysStat := fi.Sys().(*syscall.Stat_t)
-	if unix.Major(sysStat.Rdev) != 0 || unix.Minor(sysStat.Rdev) != 0 {
-		t.Fatalf("extract didn't make overlay whiteout %v", sysStat.Dev)
+	if !isOverlayWhiteout(fi) {
+		t.Fatalf("extract didn't make overlay whiteout")
 	}
 
 	if canSetTrustedXattrs {

oci/layer/types.go
@@ -41,3 +41,14 @@ type UnpackOptions struct {
 	// WhiteoutMode is the type of whiteout to write to the filesystem.
 	WhiteoutMode WhiteoutMode
 }
+
+// RepackOptions describes the behavior of the various GenerateLayer operations.
+type RepackOptions struct {
+	// MapOptions are the UID and GID mappings used when unpacking an image
+	MapOptions MapOptions
+
+	// TranslateOverlayWhiteouts changes char devices of type 0,0 to
+	// .wh.foo style whiteouts when generating tarballs. Without this,
+	// whiteouts are untouched.
+	TranslateOverlayWhiteouts bool
+}

oci/layer/utils.go
@@ -28,6 +28,7 @@ import (
 	"github.com/opencontainers/umoci/pkg/idtools"
 	"github.com/pkg/errors"
 	rootlesscontainers "github.com/rootless-containers/proto/go-proto"
+	"golang.org/x/sys/unix"
 )
 
 // MapOptions specifies the UID and GID mappings used when unpacking and
@@ -224,4 +225,15 @@ func InnerErrno(err error) error {
 		errno = err.Err
 	}
 	return errno
+}
+
+// isOverlayWhiteout returns true if the FileInfo represents an overlayfs style
+// whiteout (i.e. mknod c 0 0) and false otherwise.
+func isOverlayWhiteout(info os.FileInfo) bool {
+	sysStat := info.Sys().(*unix.Stat_t)
+	if unix.Major(uint64(sysStat.Rdev)) != 0 || unix.Minor(uint64(sysStat.Rdev)) != 0 {
+		return false
+	}
+
+	return info.Mode()&os.ModeCharDevice != 0
 }

commit 9cab21aa8282ee58efb3170afe9ecc19d392b48f

oci: generate: actually generate layer with TranslateOverlayWhiteouts
A misplaced continue statement meant that TranslateOverlayWhiteouts
would actually end up producing a layer with only overlayfs whiteout
entries and no other files.

Fixes: a425f92e9103 ("layer: add support for translating overlay whiteouts to OCI ones")
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

oci/layer/generate.go
@@ -100,8 +100,8 @@ func GenerateLayer(path string, deltas []mtree.InodeDelta, opt *RepackOptions) (
 						if err := tg.AddWhiteout(name); err != nil {
 							return fmt.Errorf("generate whiteout from overlayfs: %w", err)
 						}
+						continue
 					}
-					continue
 				}
 				if err := tg.AddFile(name, fullPath); err != nil {
 					log.Warnf("generate layer: could not add file %q: %s", name, err)

oci/layer/generate_linux_test.go
@@ -99,8 +99,11 @@ func TestGenerateLayerTranslateOverlayWhiteouts(t *testing.T) {
 	defer reader.Close()
 
 	tr := tar.NewReader(reader)
-
 	hdr, err := tr.Next()
+	assert.NoError(t, err, "read next header")
+	assert.Equal(t, hdr.Name, ".", "first entry should be /")
+
+	hdr, err = tr.Next()
 	assert.NoError(t, err, "read next header")
 	assert.EqualValues(t, hdr.Typeflag, tar.TypeReg, "whiteout typeflag")
 	assert.Equal(t, hdr.Name, whPrefix+"test", "whiteout pathname prefix")