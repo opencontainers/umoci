opencontainers/umoci

commit 9a1cefaf16bf674a7c791659fd361ff775e77efb

oci: layer: correctly handle trusted.overlay xattr namespace escaping
Since Linux 6.7[1], overlayfs supports representations of an overlayfs
lower directory in a merged overlayfs through xattr escaping. Supporting
this is preferable to silently ignoring any trusted.overlay.* xattrs we
see in an archive.

[1]: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=dad02fad84cbce30f317b69a4f2391f90045f79d

Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

oci/layer/tar_extract.go
@@ -168,15 +168,15 @@ func (te *TarExtractor) restoreMetadata(path string, hdr *tar.Header) error {
 	// want, we first clear the set of xattrs from the file then apply the ones
 	// set in the tar.Header.
 	err := te.fsEval.Lclearxattrs(path, func(xattr string) bool {
-		_, ok := ignoreXattrs[xattr]
-		return ok
+		filter, isSpecial := getXattrFilter(xattr)
+		return isSpecial && filter.MaskedOnDisk(te.whiteoutMode, xattr)
 	})
 	if err != nil {
 		if !errors.Is(err, unix.ENOTSUP) {
 			return fmt.Errorf("clear xattr metadata: %s: %w", path, err)
 		}
 		if !te.enotsupWarned {
-			log.Warnf("xattr{%s} ignoring ENOTSUP on clearxattrs", path)
+			log.Warnf("xattr{%s} ignoring ENOTSUP on clearxattrs", hdr.Name)
 			log.Warnf("xattr{%s} destination filesystem does not support xattrs, further warnings will be suppressed", path)
 			te.enotsupWarned = true
 		} else {
@@ -184,29 +184,38 @@ func (te *TarExtractor) restoreMetadata(path string, hdr *tar.Header) error {
 		}
 	}
 
-	for name, value := range hdr.Xattrs {
+	for xattr, value := range hdr.Xattrs {
 		value := []byte(value)
 
-		// Forbidden xattrs should never be touched.
-		if _, skip := ignoreXattrs[name]; skip {
-			// If the xattr is already set to the requested value, don't bail.
-			// The reason for this logic is kinda convoluted, but effectively
-			// because restoreMetadata is called with the *on-disk* metadata we
-			// run the risk of things like "security.selinux" being included in
-			// that metadata (and thus tripping the forbidden xattr error). By
-			// only touching xattrs that have a different value we are somewhat
-			// more efficient and we don't have to special case parent restore.
-			// Of course this will only ever impact ignoreXattrs.
-			if oldValue, err := te.fsEval.Lgetxattr(path, name); err == nil {
-				if bytes.Equal(value, oldValue) {
-					log.Debugf("restore xattr metadata: skipping already-set xattr %q: %s", name, hdr.Name)
-					continue
+		// Some xattrs need to be skipped for sanity reasons, such as
+		// security.selinux, because they are very much host-specific and
+		// extracting them from layers would be a really bad idea. Also, other
+		// xattrs may need to be remapped (such as trusted.overlay.* xattrs
+		// when in overlayfs mode) to have correct values.
+		mappedName := xattr
+		if filter, isSpecial := getXattrFilter(xattr); isSpecial {
+			if newName := filter.ToDisk(te.whiteoutMode, xattr); newName == nil {
+				// Avoid outputting a warning if a must-skip xattr already has
+				// the expected value we wanted.
+				//
+				// TODO: Maybe we should still emit a warning even in this case
+				//       because now that the directory has its xattrs cleared
+				//       with ToTar, we should probably warn if images have
+				//       xattrs that only happen to be applied correctly now.
+				if oldValue, err := te.fsEval.Lgetxattr(path, xattr); err == nil {
+					if bytes.Equal(value, oldValue) {
+						log.Debugf("xattr{%s} restore xattr metadata: skipping already-set xattr %q", hdr.Name, xattr)
+						continue
+					}
 				}
+				log.Warnf("xattr{%s} ignoring forbidden xattr: %q", hdr.Name, xattr)
+				continue
+			} else if *newName != xattr {
+				mappedName = *newName
+				log.Debugf("xattr{%s} remapping xattr %q to %q during extraction", hdr.Name, xattr, mappedName)
 			}
-			log.Warnf("xattr{%s} ignoring forbidden xattr: %q", hdr.Name, name)
-			continue
 		}
-		if err := te.fsEval.Lsetxattr(path, name, value, 0); err != nil {
+		if err := te.fsEval.Lsetxattr(path, mappedName, value, 0); err != nil {
 			// In rootless mode, some xattrs will fail (security.capability).
 			// This is _fine_ as long as we're not running as root (in which
 			// case we shouldn't be ignoring xattrs that we were told to set).
@@ -216,7 +225,7 @@ func (te *TarExtractor) restoreMetadata(path string, hdr *tar.Header) error {
 			//       unprivileged users (we also would need to translate them
 			//       back when creating archives).
 			if te.partialRootless && errors.Is(err, os.ErrPermission) {
-				log.Warnf("rootless{%s} ignoring (usually) harmless EPERM on setxattr %q", hdr.Name, name)
+				log.Warnf("rootless{%s} ignoring (usually) harmless EPERM on setxattr %q", hdr.Name, mappedName)
 				continue
 			}
 			// We cannot do much if we get an ENOTSUP -- this usually means
@@ -224,7 +233,7 @@ func (te *TarExtractor) restoreMetadata(path string, hdr *tar.Header) error {
 			// underlying filesystem (such as AUFS or NFS).
 			if errors.Is(err, unix.ENOTSUP) {
 				if !te.enotsupWarned {
-					log.Warnf("xattr{%s} ignoring ENOTSUP on setxattr %q", hdr.Name, name)
+					log.Warnf("xattr{%s} ignoring ENOTSUP on setxattr %q", hdr.Name, mappedName)
 					log.Warnf("xattr{%s} destination filesystem does not support xattrs, further warnings will be suppressed", path)
 					te.enotsupWarned = true
 				} else {
@@ -555,7 +564,33 @@ func (te *TarExtractor) UnpackEntry(root string, hdr *tar.Header, r io.Reader) (
 				if err != nil {
 					return fmt.Errorf("get xattr: %w", err)
 				}
-				dirHdr.Xattrs[xattr] = string(value)
+				// Because restoreMetadata will re-apply these xattrs
+				// (potentially remapping them if we have specialXattrs
+				// filters) we need to map their names to match what we would
+				// get from an actual archive.
+				//
+				// However, since these are xattrs on the underlying filesystem
+				// we don't need to provide any user warnings.
+				mappedName := xattr
+				if filter, isSpecial := getXattrFilter(xattr); isSpecial {
+					if newName := filter.ToTar(te.whiteoutMode, xattr); newName == nil {
+						// If the xattr should be ignored we can safely skip it
+						// here because MaskedOnDisk will also stop them from
+						// being cleared. However, just to be safe we should
+						// verify that this is actually true (otherwise you'll
+						// end up with silently wrong extractions).
+						if filter.MaskedOnDisk(te.whiteoutMode, xattr) {
+							// TODO: Find a nicer setup that doesn't require
+							// this fatal error.
+							log.Fatalf("[internal error] xattr %q is being hidden by GenerateEntry but UnpackShouldClear returns true")
+						}
+						continue
+					} else if *newName != xattr {
+						mappedName = *newName
+						log.Debugf("xattr{%s} remapping xattr %q to %q for later restoreMetadata", unsafeDir, xattr, mappedName)
+					}
+				}
+				dirHdr.Xattrs[mappedName] = string(value)
 			}
 		}
 

oci/layer/tar_generate.go
@@ -32,49 +32,7 @@ import (
 
 	"github.com/opencontainers/umoci/pkg/fseval"
 	"github.com/opencontainers/umoci/pkg/system"
-	"github.com/opencontainers/umoci/pkg/testutils"
 )
-
-// ignoreXattrs is a list of xattr names that should be ignored when
-// creating a new image layer, because they are host-specific and/or would be a
-// bad idea to unpack. They are also excluded from Lclearxattr when extracting
-// an archive.
-// XXX: Maybe we should make this configurable so users can manually blacklist
-//
-//	(or even whitelist) xattrs that they actually want included? Like how
-//	GNU tar's xattr setup works.
-var ignoreXattrs = map[string]struct{}{
-	// SELinux doesn't allow you to set SELinux policies generically. They're
-	// also host-specific. So just ignore them during extraction.
-	"security.selinux": {},
-
-	// NFSv4 ACLs are very system-specific and shouldn't be touched by us, nor
-	// should they be included in images.
-	"system.nfs4_acl": {},
-
-	// In order to support overlayfs whiteout mode, we shouldn't un-set
-	// this after we've set it when writing out the whiteouts.
-	"trusted.overlay.opaque": {},
-
-	// We don't want to these xattrs into the image, because they're only
-	// relevant based on how the build overlay is constructed and will not
-	// be true on the target system once the image is unpacked (e.g. inodes
-	// might be different, impure status won't be true, etc.).
-	"trusted.overlay.redirect": {},
-	"trusted.overlay.origin":   {},
-	"trusted.overlay.impure":   {},
-	"trusted.overlay.nlink":    {},
-	"trusted.overlay.upper":    {},
-	"trusted.overlay.metacopy": {},
-}
-
-func init() {
-	// For test purposes we add a fake forbidden attribute that an unprivileged
-	// user can easily write to (and thus we can test it).
-	if testutils.IsTestBinary() {
-		ignoreXattrs["user.UMOCI:forbidden_xattr"] = struct{}{}
-	}
-}
 
 // tarGenerator is a helper for generating layer diff tars. It should be noted
 // that when using tarGenerator.Add{Path,Whiteout} it is recommended to do it
@@ -212,33 +170,42 @@ func (tg *tarGenerator) AddFile(name, path string) error {
 	// Set up xattrs externally to updateHeader because the function signature
 	// would look really dumb otherwise.
 	// XXX: This should probably be moved to a function in tar_unix.go.
-	names, err := tg.fsEval.Llistxattr(path)
+	xattrs, err := tg.fsEval.Llistxattr(path)
 	if err != nil {
 		if !errors.Is(err, unix.EOPNOTSUPP) {
 			return fmt.Errorf("get xattr list: %w", err)
 		}
-		names = []string{}
+		xattrs = []string{}
 	}
-	for _, name := range names {
+	for _, xattr := range xattrs {
 		// Some xattrs need to be skipped for sanity reasons, such as
 		// security.selinux, because they are very much host-specific and
-		// carrying them to other hosts would be a really bad idea.
-		if _, ignore := ignoreXattrs[name]; ignore {
-			continue
+		// carrying them to other hosts would be a really bad idea. Other
+		// xattrs need to be remapped (such as escaped trusted.overlay.* xattrs
+		// when in overlayfs mode) to have correct values.
+		mappedName := xattr
+		if filter, isSpecial := getXattrFilter(xattr); isSpecial {
+			if newName := filter.ToTar(tg.whiteoutMode, xattr); newName == nil {
+				log.Debugf("xattr{%s} skipping the inclusion of xattr %q in generated tar archive", hdr.Name, xattr)
+				continue
+			} else if *newName != xattr {
+				mappedName = *newName
+				log.Debugf("xattr{%s} remapping xattr %q to %q in generated tar archive", hdr.Name, xattr, mappedName)
+			}
 		}
 		// TODO: We should translate all v3 capabilities into root-owned
 		//       capabilities here. But we don't have Go code for that yet
 		//       (we'd need to use libcap to parse it).
-		value, err := tg.fsEval.Lgetxattr(path, name)
+		value, err := tg.fsEval.Lgetxattr(path, xattr)
 		if err != nil {
 			// Ignore xattrs we were unable to read or if the filesystem is
 			// refusing to provide information about them. Note that rather
 			// than getting a permission error when reading a trusted.* xattr
 			// as an unprivileged user, you actually get ENODATA.
-			log.Debugf("failure reading xattr from list on %q: %q", name, err)
+			log.Debugf("xattr{%s} failure reading xattr %q: %v", hdr.Name, xattr, err)
 			// TODO: Should we use errors.As?
 			if !errors.Is(err, unix.EOPNOTSUPP) && !errors.Is(err, unix.ENODATA) {
-				return fmt.Errorf("get xattr: %s: %w", name, err)
+				return fmt.Errorf("get xattr %q: %w", xattr, err)
 			}
 		}
 		// https://golang.org/issues/20698 -- We don't just error out here
@@ -246,12 +213,12 @@ func (tg *tarGenerator) AddFile(name, path string) error {
 		// whether the stdlib will correctly handle reading or disable writing
 		// of these PAX headers so we have to track this ourselves.
 		if len(value) <= 0 {
-			log.Warnf("ignoring empty-valued xattr %s: disallowed by PAX standard", name)
+			log.Warnf("xattr{%s} ignoring empty-valued xattr %q: disallowed by PAX standard", hdr.Name, xattr)
 			continue
 		}
 		// Note that Go strings can actually be arbitrary byte sequences, so
 		// this conversion (while it might look a bit wrong) is actually fine.
-		hdr.Xattrs[name] = string(value)
+		hdr.Xattrs[mappedName] = string(value)
 	}
 
 	// Not all systems have the concept of an inode, but I'm not in the mood to

oci/layer/xattr.go
@@ -1,0 +1,215 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+ * umoci: Umoci Modifies Open Containers' Images
+ * Copyright (C) 2016-2025 SUSE LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package layer
+
+import (
+	"strings"
+
+	"github.com/apex/log"
+
+	"github.com/opencontainers/umoci/pkg/testutils"
+)
+
+// TODO: These method names are confusing since GenerateEntry() is called
+// during extraction to make restoreMetadata() not break things...
+
+// xattrFilter is used to modify xattrs during layer generation and extraction.
+// Very simple filters may completely block the layer generation and extraction
+// code from seeing certain system xattrs (such as security.selinux and
+// security.nfs4_acl), while others may require xattr names to be remapped
+// (such as with trusted.overlay.*).
+//
+// [ToTar] is conceptually the inverse of [ToDisk], though both can remove
+// xattrs entirely. Some key properties that [ToTar] implementations must
+// ensure are:
+//
+//   - For xattrs where ToTar(xattr) != nil, implementations must ensure that
+//     ToDisk(ToTar(xattr)) === xattr.
+//
+//   - For a given xattr and [WhiteoutMode], [MaskedOnDisk] must return true
+//     (meaning that the on-disk xattr will NOT be cleared) *if and only if*
+//     [ToTar] will returns nil.
+//
+// For xattrs that are not purely masked, you can think of [ToTar] as being
+// "unescape", and [ToDisk] being "escape", and [MaskedOnDisk] indicating
+// whether the xattr is something that [ToDisk] would *not* generate and
+// [ToTar] would
+// ignore.
+type xattrFilter interface {
+	// MaskedOnDisk indicates whether the given xattr should be hidden from
+	// code that iterates over on-disk xattrs (namely lclearxattrs). Note that
+	// we will (during the normal course of extraction) clear xattrs and
+	// re-apply them from tar archives, so xattrs should only be masked if
+	// umoci is simply not meant to interact with a given xattr.
+	//
+	// In particular, due to the implementation details of UnpackEntry
+	// (described in [ToTar]), MaskedOnDisk must only return true *if and only
+	// if* (for the same xattr and whiteout mode) [ToTar] would return nil and
+	// [ToDisk] would never generate it. Otherwise, it is impossible for this
+	// on-disk xattr to have come from or be stored in the archive.
+	MaskedOnDisk(onDiskMode WhiteoutMode, xattr string) bool
+
+	// ToDisk returns what name a tar archive xattr should be stored with
+	// on-disk (with the provided [WhiteoutMode] as the "on disk" format). The
+	// returned string will be used for as the on-disk xattr name instead of
+	// the original xattr -- if nil is returned then the xattr will not be
+	// extracted.
+	//
+	// [MaskedOnDisk] must only return true if [ToDisk] would never generate
+	// the same xattr.
+	ToDisk(onDiskMode WhiteoutMode, xattr string) (newName *string)
+
+	// ToTar returns what an on-disk xattr name should be converted to when
+	// storing in a tar archive (with the provided [WhiteoutMode] as the "on
+	// disk" format). The returned string will be stored inside tar archives as
+	// the xattr name instead of the original xattr -- if nil is returned then
+	// the xattr will not be included in the tar archive.
+	//
+	// Note that ToTar is not exclusively called on tar archive entries from
+	// layer archives. When doing UnpackEntry we need to restore the metadata
+	// of the parent directory of any path we extract into (to ensure that
+	// {a,c,m}times are correct), and an implementation detail of UnpackEntry
+	// is that this involves converting the on-disk information of the parent
+	// directory into a tar.Header and then back into the on-disk
+	// representation (to ensure that extracting entry metadata is identical to
+	// restoring parent directory metadata). This means that ToTar will be
+	// routinely called on directories that may have "special" xattrs.
+	//
+	// [MaskedOnDisk] must only return true for a given xattr and
+	// [WhiteoutMode] *if and only if*, ToTar with the same arguments returns
+	// nil. Otherwise, when the metadata is re-applied with on-disk data you
+	// will end up losing on-disk xattrs or adding new nonsense xattrs.
+	ToTar(onDiskMode WhiteoutMode, xattr string) (newName *string)
+}
+
+// forbiddenXattrFilter is a dummy filter that will block all xattrs that are
+// associated with it.
+type forbiddenXattrFilter struct{}
+
+var _ xattrFilter = forbiddenXattrFilter{}
+
+func (_ forbiddenXattrFilter) MaskedOnDisk(_ WhiteoutMode, _ string) bool { return true }
+func (_ forbiddenXattrFilter) ToDisk(_ WhiteoutMode, _ string) *string    { return nil }
+func (_ forbiddenXattrFilter) ToTar(_ WhiteoutMode, _ string) *string     { return nil }
+
+// overlayXattrFilter is a filter for all trusted.overlay.* xattrs which will
+// escape the xattrs on unpack and unescape them when
+type overlayXattrFilter struct{}
+
+var _ xattrFilter = overlayXattrFilter{}
+
+func (_ overlayXattrFilter) MaskedOnDisk(woMode WhiteoutMode, xattr string) bool {
+	// Only trusted.overlay.* top-level xattrs are masked in overlayfs mode.
+	// Escaped xattrs and xattrs in regular mode are allowed.
+	return woMode == OverlayFSWhiteout &&
+		doesXattrMatch(xattr, "trusted.overlay.") &&
+		!doesXattrMatch(xattr, "trusted.overlay.overlay.")
+}
+
+func (_ overlayXattrFilter) ToDisk(woMode WhiteoutMode, xattr string) *string {
+	if woMode != OverlayFSWhiteout {
+		// In non-overlayfs mode, overlay xattrs are not special and can be
+		// treated like any other xattr. (Though it would be a little strange
+		// to see them.)
+		return &xattr
+	}
+
+	subXattr, found := strings.CutPrefix(xattr, "trusted.overlay.")
+	if !found {
+		// We should never hit this case in practice.
+		// TODO: Should we just panic here?
+		log.Warnf("[internal error] overlayfs xattr filter called for non-overlayfs xattr %q", xattr)
+		return &xattr
+	}
+	escaped := "trusted.overlay.overlay." + subXattr
+	return &escaped
+}
+
+func (_ overlayXattrFilter) ToTar(woMode WhiteoutMode, xattr string) *string {
+	if woMode != OverlayFSWhiteout {
+		// In non-overlayfs mode, overlay xattrs are not special and can be
+		// treated like any other xattr. (Though it would be a little strange
+		// to see them.)
+		return &xattr
+	}
+
+	subXattr, isEscapedXattr := strings.CutPrefix(xattr, "trusted.overlay.overlay.")
+	if !isEscapedXattr {
+		// Clear any non-escaped xattrs entirely, as they may have been
+		// auto-set by overlayfs or set by the user when configuring overlayfs.
+		// This matches the behaviour of MaskedOnDisk.
+		return nil
+	}
+	unescaped := "trusted.overlay." + subXattr
+	return &unescaped
+}
+
+// specialXattrs is a list of xattr names (or prefixes) that may need have to
+// have special handling because treating them as-is would be incorrect (either
+// because they are host-specific and need to be hidden from images or would
+// result in counter-intuitive behaviour).
+//
+// TODO: Maybe we should make this configurable so users can manually blacklist
+// (or even whitelist) xattrs that they actually want included? Like how GNU
+// tar's xattr setup works.
+var specialXattrs = map[string]xattrFilter{
+	// SELinux doesn't allow you to set SELinux policies generically. They're
+	// also host-specific. So just ignore them during extraction.
+	"security.selinux": forbiddenXattrFilter{},
+
+	// NFSv4 ACLs are very system-specific and shouldn't be touched by us, nor
+	// should they be included in images.
+	"system.nfs4_acl": forbiddenXattrFilter{},
+
+	// The overlayfs namespace of xattrs need to have special handling when
+	// operating with the overlayfs on-disk format.
+	"trusted.overlay.": overlayXattrFilter{},
+}
+
+func init() {
+	// For test purposes we add a fake forbidden attribute that an unprivileged
+	// user can easily write to (and thus we can test it).
+	if testutils.IsTestBinary() {
+		specialXattrs["user.UMOCI:forbidden_xattr"] = forbiddenXattrFilter{}
+	}
+}
+
+// doesXattrMatch returns whether the given xattr matches the filter. The
+// semantics are very simple -- if the filter ends with "." then it is treated
+// as a prefix while if it doesn't end with "." it must match exactly.
+func doesXattrMatch(xattr, filter string) bool {
+	return filter == xattr ||
+		(strings.HasSuffix(filter, ".") && strings.HasPrefix(xattr, filter))
+}
+
+// getXattrFilter looks for the filter which matches xattr. isSpecial will be
+// true if there is a registered filter that matches the provided xattr.
+func getXattrFilter(xattr string) (filter xattrFilter, isSpecial bool) {
+	// fast path: look up the xattr directly
+	if filter, ok := specialXattrs[xattr]; ok {
+		return filter, ok
+	}
+	// slow path: match xattr prefixes
+	for prefix, filter := range specialXattrs {
+		if doesXattrMatch(xattr, prefix) {
+			return filter, true
+		}
+	}
+	return nil, false
+}

commit caa0306d0f2d6f74b85037d4a0df0712200abd74

oci: layer: improve logging of xattr mapping
We forgot to output the problematic xattr when doing log.Fatalf. Oops.

Fixes: 9a1cefaf16bf ("oci: layer: correctly handle trusted.overlay xattr namespace escaping")
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

oci/layer/tar_extract.go
@@ -208,7 +208,7 @@ func (te *TarExtractor) restoreMetadata(path string, hdr *tar.Header) error {
 						continue
 					}
 				}
-				log.Warnf("xattr{%s} ignoring forbidden xattr: %q", hdr.Name, xattr)
+				log.Warnf("xattr{%s} ignoring forbidden xattr %q", hdr.Name, xattr)
 				continue
 			} else if *newName != xattr {
 				mappedName = *newName
@@ -535,6 +535,7 @@ func (te *TarExtractor) UnpackEntry(root string, hdr *tar.Header, r io.Reader) (
 		}
 
 		// More faking to trick restoreMetadata to actually restore the directory.
+		dirHdr.Name = unsafeDir
 		dirHdr.Typeflag = tar.TypeDir
 		dirHdr.Linkname = ""
 
@@ -574,6 +575,7 @@ func (te *TarExtractor) UnpackEntry(root string, hdr *tar.Header, r io.Reader) (
 				mappedName := xattr
 				if filter, isSpecial := getXattrFilter(xattr); isSpecial {
 					if newName := filter.ToTar(te.whiteoutMode, xattr); newName == nil {
+						log.Debugf("xattr{%s} ignoring masked xattr %q while generating fake parent directory header for restoreMetadata", unsafeDir, xattr)
 						// If the xattr should be ignored we can safely skip it
 						// here because MaskedOnDisk will also stop them from
 						// being cleared. However, just to be safe we should
@@ -582,7 +584,7 @@ func (te *TarExtractor) UnpackEntry(root string, hdr *tar.Header, r io.Reader) (
 						if filter.MaskedOnDisk(te.whiteoutMode, xattr) {
 							// TODO: Find a nicer setup that doesn't require
 							// this fatal error.
-							log.Fatalf("[internal error] xattr %q is being hidden by GenerateEntry but UnpackShouldClear returns true")
+							log.Fatalf("[internal error] xattr{%s} masked %q is being hidden by (%T).GenerateEntry but UnpackShouldClear returns true", unsafeDir, xattr, filter)
 						}
 						continue
 					} else if *newName != xattr {