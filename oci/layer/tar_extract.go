opencontainers/umoci

commit 9a1cefaf16bf674a7c791659fd361ff775e77efb

oci: layer: correctly handle trusted.overlay xattr namespace escaping
Since Linux 6.7[1], overlayfs supports representations of an overlayfs
lower directory in a merged overlayfs through xattr escaping. Supporting
this is preferable to silently ignoring any trusted.overlay.* xattrs we
see in an archive.

[1]: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=dad02fad84cbce30f317b69a4f2391f90045f79d

Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

oci/layer/tar_extract.go
@@ -168,15 +168,15 @@ func (te *TarExtractor) restoreMetadata(path string, hdr *tar.Header) error {
 	// want, we first clear the set of xattrs from the file then apply the ones
 	// set in the tar.Header.
 	err := te.fsEval.Lclearxattrs(path, func(xattr string) bool {
-		_, ok := ignoreXattrs[xattr]
-		return ok
+		filter, isSpecial := getXattrFilter(xattr)
+		return isSpecial && filter.MaskedOnDisk(te.whiteoutMode, xattr)
 	})
 	if err != nil {
 		if !errors.Is(err, unix.ENOTSUP) {
 			return fmt.Errorf("clear xattr metadata: %s: %w", path, err)
 		}
 		if !te.enotsupWarned {
-			log.Warnf("xattr{%s} ignoring ENOTSUP on clearxattrs", path)
+			log.Warnf("xattr{%s} ignoring ENOTSUP on clearxattrs", hdr.Name)
 			log.Warnf("xattr{%s} destination filesystem does not support xattrs, further warnings will be suppressed", path)
 			te.enotsupWarned = true
 		} else {
@@ -184,29 +184,38 @@ func (te *TarExtractor) restoreMetadata(path string, hdr *tar.Header) error {
 		}
 	}
 
-	for name, value := range hdr.Xattrs {
+	for xattr, value := range hdr.Xattrs {
 		value := []byte(value)
 
-		// Forbidden xattrs should never be touched.
-		if _, skip := ignoreXattrs[name]; skip {
-			// If the xattr is already set to the requested value, don't bail.
-			// The reason for this logic is kinda convoluted, but effectively
-			// because restoreMetadata is called with the *on-disk* metadata we
-			// run the risk of things like "security.selinux" being included in
-			// that metadata (and thus tripping the forbidden xattr error). By
-			// only touching xattrs that have a different value we are somewhat
-			// more efficient and we don't have to special case parent restore.
-			// Of course this will only ever impact ignoreXattrs.
-			if oldValue, err := te.fsEval.Lgetxattr(path, name); err == nil {
-				if bytes.Equal(value, oldValue) {
-					log.Debugf("restore xattr metadata: skipping already-set xattr %q: %s", name, hdr.Name)
-					continue
+		// Some xattrs need to be skipped for sanity reasons, such as
+		// security.selinux, because they are very much host-specific and
+		// extracting them from layers would be a really bad idea. Also, other
+		// xattrs may need to be remapped (such as trusted.overlay.* xattrs
+		// when in overlayfs mode) to have correct values.
+		mappedName := xattr
+		if filter, isSpecial := getXattrFilter(xattr); isSpecial {
+			if newName := filter.ToDisk(te.whiteoutMode, xattr); newName == nil {
+				// Avoid outputting a warning if a must-skip xattr already has
+				// the expected value we wanted.
+				//
+				// TODO: Maybe we should still emit a warning even in this case
+				//       because now that the directory has its xattrs cleared
+				//       with ToTar, we should probably warn if images have
+				//       xattrs that only happen to be applied correctly now.
+				if oldValue, err := te.fsEval.Lgetxattr(path, xattr); err == nil {
+					if bytes.Equal(value, oldValue) {
+						log.Debugf("xattr{%s} restore xattr metadata: skipping already-set xattr %q", hdr.Name, xattr)
+						continue
+					}
 				}
+				log.Warnf("xattr{%s} ignoring forbidden xattr: %q", hdr.Name, xattr)
+				continue
+			} else if *newName != xattr {
+				mappedName = *newName
+				log.Debugf("xattr{%s} remapping xattr %q to %q during extraction", hdr.Name, xattr, mappedName)
 			}
-			log.Warnf("xattr{%s} ignoring forbidden xattr: %q", hdr.Name, name)
-			continue
 		}
-		if err := te.fsEval.Lsetxattr(path, name, value, 0); err != nil {
+		if err := te.fsEval.Lsetxattr(path, mappedName, value, 0); err != nil {
 			// In rootless mode, some xattrs will fail (security.capability).
 			// This is _fine_ as long as we're not running as root (in which
 			// case we shouldn't be ignoring xattrs that we were told to set).
@@ -216,7 +225,7 @@ func (te *TarExtractor) restoreMetadata(path string, hdr *tar.Header) error {
 			//       unprivileged users (we also would need to translate them
 			//       back when creating archives).
 			if te.partialRootless && errors.Is(err, os.ErrPermission) {
-				log.Warnf("rootless{%s} ignoring (usually) harmless EPERM on setxattr %q", hdr.Name, name)
+				log.Warnf("rootless{%s} ignoring (usually) harmless EPERM on setxattr %q", hdr.Name, mappedName)
 				continue
 			}
 			// We cannot do much if we get an ENOTSUP -- this usually means
@@ -224,7 +233,7 @@ func (te *TarExtractor) restoreMetadata(path string, hdr *tar.Header) error {
 			// underlying filesystem (such as AUFS or NFS).
 			if errors.Is(err, unix.ENOTSUP) {
 				if !te.enotsupWarned {
-					log.Warnf("xattr{%s} ignoring ENOTSUP on setxattr %q", hdr.Name, name)
+					log.Warnf("xattr{%s} ignoring ENOTSUP on setxattr %q", hdr.Name, mappedName)
 					log.Warnf("xattr{%s} destination filesystem does not support xattrs, further warnings will be suppressed", path)
 					te.enotsupWarned = true
 				} else {
@@ -555,7 +564,33 @@ func (te *TarExtractor) UnpackEntry(root string, hdr *tar.Header, r io.Reader) (
 				if err != nil {
 					return fmt.Errorf("get xattr: %w", err)
 				}
-				dirHdr.Xattrs[xattr] = string(value)
+				// Because restoreMetadata will re-apply these xattrs
+				// (potentially remapping them if we have specialXattrs
+				// filters) we need to map their names to match what we would
+				// get from an actual archive.
+				//
+				// However, since these are xattrs on the underlying filesystem
+				// we don't need to provide any user warnings.
+				mappedName := xattr
+				if filter, isSpecial := getXattrFilter(xattr); isSpecial {
+					if newName := filter.ToTar(te.whiteoutMode, xattr); newName == nil {
+						// If the xattr should be ignored we can safely skip it
+						// here because MaskedOnDisk will also stop them from
+						// being cleared. However, just to be safe we should
+						// verify that this is actually true (otherwise you'll
+						// end up with silently wrong extractions).
+						if filter.MaskedOnDisk(te.whiteoutMode, xattr) {
+							// TODO: Find a nicer setup that doesn't require
+							// this fatal error.
+							log.Fatalf("[internal error] xattr %q is being hidden by GenerateEntry but UnpackShouldClear returns true")
+						}
+						continue
+					} else if *newName != xattr {
+						mappedName = *newName
+						log.Debugf("xattr{%s} remapping xattr %q to %q for later restoreMetadata", unsafeDir, xattr, mappedName)
+					}
+				}
+				dirHdr.Xattrs[mappedName] = string(value)
 			}
 		}
 

oci/layer/xattr_linux_test.go
@@ -1,0 +1,268 @@
+//go:build linux
+// +build linux
+
+// SPDX-License-Identifier: Apache-2.0
+/*
+ * umoci: Umoci Modifies Open Containers' Images
+ * Copyright (C) 2016-2025 SUSE LLC
+ * Copyright (C) 2020 Cisco Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package layer
+
+import (
+	"archive/tar"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/vbatts/go-mtree"
+
+	"github.com/opencontainers/umoci/pkg/fseval"
+	"github.com/opencontainers/umoci/pkg/system"
+)
+
+func getAllXattrs(t *testing.T, path string) map[string]string {
+	names, err := system.Llistxattr(path)
+	require.NoErrorf(t, err, "fetch all xattrs for %q", path)
+	xattrs := map[string]string{}
+	for _, name := range names {
+		value, err := system.Lgetxattr(path, name)
+		require.NoErrorf(t, err, "fetch xattr %q for %q", name, path)
+		xattrs[name] = string(value)
+	}
+	return xattrs
+}
+
+func TestUnpackGenerateRoundTrip_ComplexXattr_OverlayFS(t *testing.T) {
+	dir := t.TempDir()
+
+	testNeedsMknod(t)
+	testNeedsTrustedOverlayXattrs(t)
+
+	dentries := []struct {
+		tarDentry
+		remapXattrs map[string]string
+	}{
+		{
+			tarDentry{path: ".", ftype: tar.TypeDir, xattrs: map[string]string{
+				"trusted.overlay.opaque": "x",
+				"user.dummy.xattr":       "foobar",
+			}},
+			map[string]string{
+				"trusted.overlay.overlay.opaque": "x",
+				"user.dummy.xattr":               "foobar",
+			},
+		},
+		// Set a fake overlayfs xattr in the trusted.overlay namespace on a
+		// directory that contains entries. Since restoreMetadata() gets called
+		// on all parent directories when unpacking, this will cause
+		// restoreMetadata() to be run on the same directory multiple times.
+		// This lets us test that restoreMetadata will not re-apply the xattr
+		// escaping even after being called multiple times.
+		{
+			tarDentry{path: "foo/", ftype: tar.TypeDir, xattrs: map[string]string{
+				"trusted.overlay.fakexattr": "fakexattr",
+			}},
+			map[string]string{
+				"trusted.overlay.overlay.fakexattr": "fakexattr",
+			},
+		},
+		// Some subpaths with dummy overlayfs xattrs.
+		{
+			tarDentry{path: "foo/bar", ftype: tar.TypeReg, contents: "file", xattrs: map[string]string{
+				"trusted.overlay.whiteout": "foo",
+			}},
+			map[string]string{
+				"trusted.overlay.overlay.whiteout": "foo",
+			},
+		},
+		{
+			tarDentry{path: "foo/baz/", ftype: tar.TypeDir, xattrs: map[string]string{
+				"trusted.overlay.opaque": "y",
+			}},
+			map[string]string{
+				"trusted.overlay.overlay.opaque": "y",
+			},
+		},
+		// Several levels nested overlayfs xattrs.
+		{
+			tarDentry{path: "foo/extra-nesting/", ftype: tar.TypeDir, xattrs: map[string]string{
+				"trusted.overlay.overlay.opaque":                                "x",
+				"trusted.overlay.overlay.overlay.whiteout":                      "foobar",
+				"trusted.overlay.overlay.overlay.overlay.overlay.overlay.dummy": "dummy xattr",
+			}},
+			map[string]string{
+				"trusted.overlay.overlay.overlay.opaque":                                "x",
+				"trusted.overlay.overlay.overlay.overlay.whiteout":                      "foobar",
+				"trusted.overlay.overlay.overlay.overlay.overlay.overlay.overlay.dummy": "dummy xattr",
+			},
+		},
+		{
+			tarDentry{path: "foo/extra-nesting/reg", ftype: tar.TypeReg, contents: "reg", xattrs: map[string]string{
+				"trusted.overlay.overlay.overlay.overlay.overlay.dummy123": "dummy xattr 123",
+			}},
+			map[string]string{
+				"trusted.overlay.overlay.overlay.overlay.overlay.overlay.dummy123": "dummy xattr 123",
+			},
+		},
+	}
+
+	// With extraction using OverlayFSWhiteout we expect to get the remapped
+	// xattrs.
+	t.Run("OverlayFSWhiteout", func(t *testing.T) {
+		unpackOptions := UnpackOptions{
+			MapOptions: MapOptions{
+				Rootless: os.Geteuid() != 0,
+			},
+			WhiteoutMode: OverlayFSWhiteout,
+		}
+
+		te := NewTarExtractor(unpackOptions)
+
+		for _, de := range dentries {
+			hdr, rdr := tarFromDentry(de.tarDentry)
+			err := te.UnpackEntry(dir, hdr, rdr)
+			assert.NoErrorf(t, err, "UnpackEntry %s", hdr.Name)
+		}
+
+		for _, de := range dentries {
+			path := de.path
+			fullPath := filepath.Join(dir, path)
+
+			xattrs := getAllXattrs(t, fullPath)
+			assert.Equalf(t, de.remapXattrs, xattrs, "UnpackEntry(%q): expected to see %#v remapped properly", path, de.xattrs)
+
+			// Make sure none of the files are whiteouts.
+			_, isWo, err := isOverlayWhiteout(fullPath, fseval.Default)
+			require.NoErrorf(t, err, "isOverlayWhiteout(%q)", path)
+			assert.Falsef(t, isWo, "isOverlayWhiteout(%q): regular entries with overlayfs xattrs should not end up being unpacked with overlayfs whiteout xattrs", path)
+		}
+
+		t.Run("ToGenerateLayer", func(t *testing.T) {
+			// something reasonable
+			mtreeKeywords := []mtree.Keyword{
+				"size",
+				"type",
+				"uid",
+				"gid",
+				"mode",
+			}
+			deltas, err := mtree.Check(dir, nil, mtreeKeywords, fseval.Default)
+			assert.NoError(t, err, "mtree check")
+
+			reader, err := GenerateLayer(dir, deltas, &RepackOptions{
+				TranslateOverlayWhiteouts: unpackOptions.WhiteoutMode == OverlayFSWhiteout,
+			})
+			require.NoError(t, err, "generate layer")
+			defer reader.Close()
+
+			// We expect to get the exact same thing as the original archive
+			// entries in the new archive.
+			var wantDentries []tarDentry
+			for _, dentry := range dentries {
+				wantDentries = append(wantDentries, dentry.tarDentry)
+			}
+			checkLayerEntries(t, reader, wantDentries)
+		})
+
+		t.Run("ToGenerateInsertLayer", func(t *testing.T) {
+			reader := GenerateInsertLayer(dir, ".", false, &RepackOptions{
+				TranslateOverlayWhiteouts: unpackOptions.WhiteoutMode == OverlayFSWhiteout,
+			})
+			defer reader.Close()
+
+			// We expect to get the exact same thing as the original archive
+			// entries in the new archive.
+			var wantDentries []tarDentry
+			for _, dentry := range dentries {
+				wantDentries = append(wantDentries, dentry.tarDentry)
+			}
+			checkLayerEntries(t, reader, wantDentries)
+		})
+	})
+
+	// With extraction using OverlayFSWhiteout we expect to get the remapped
+	// xattrs.
+	t.Run("OCIStandardWhiteout", func(t *testing.T) {
+		unpackOptions := UnpackOptions{
+			MapOptions: MapOptions{
+				Rootless: os.Geteuid() != 0,
+			},
+			WhiteoutMode: OCIStandardWhiteout,
+		}
+
+		te := NewTarExtractor(unpackOptions)
+
+		for _, de := range dentries {
+			hdr, rdr := tarFromDentry(de.tarDentry)
+			err := te.UnpackEntry(dir, hdr, rdr)
+			assert.NoErrorf(t, err, "UnpackEntry %s", hdr.Name)
+		}
+
+		for _, de := range dentries {
+			path := de.tarDentry.path
+			fullPath := filepath.Join(dir, path)
+
+			xattrs := getAllXattrs(t, fullPath)
+			assert.Equalf(t, de.xattrs, xattrs, "UnpackEntry(%q): expected to see %#v not be remapped", path, de.xattrs)
+			assert.NotEqualf(t, de.remapXattrs, xattrs, "UnpackEntry(%q): expected to see %#v not be remapped", path, de.xattrs)
+		}
+
+		t.Run("ToGenerateLayer", func(t *testing.T) {
+			// something reasonable
+			mtreeKeywords := []mtree.Keyword{
+				"size",
+				"type",
+				"uid",
+				"gid",
+				"mode",
+			}
+			deltas, err := mtree.Check(dir, nil, mtreeKeywords, fseval.Default)
+			assert.NoError(t, err, "mtree check")
+
+			reader, err := GenerateLayer(dir, deltas, &RepackOptions{
+				TranslateOverlayWhiteouts: unpackOptions.WhiteoutMode == OverlayFSWhiteout,
+			})
+			require.NoError(t, err, "generate layer")
+			defer reader.Close()
+
+			// We expect to get the exact same thing as the original archive
+			// entries in the new archive.
+			var wantDentries []tarDentry
+			for _, dentry := range dentries {
+				wantDentries = append(wantDentries, dentry.tarDentry)
+			}
+			checkLayerEntries(t, reader, wantDentries)
+		})
+
+		t.Run("ToGenerateInsertLayer", func(t *testing.T) {
+			reader := GenerateInsertLayer(dir, ".", false, &RepackOptions{
+				TranslateOverlayWhiteouts: unpackOptions.WhiteoutMode == OverlayFSWhiteout,
+			})
+			defer reader.Close()
+
+			// We expect to get the exact same thing as the original archive
+			// entries in the new archive.
+			var wantDentries []tarDentry
+			for _, dentry := range dentries {
+				wantDentries = append(wantDentries, dentry.tarDentry)
+			}
+			checkLayerEntries(t, reader, wantDentries)
+		})
+	})
+}

oci/layer/tar_generate.go
@@ -32,49 +32,7 @@ import (
 
 	"github.com/opencontainers/umoci/pkg/fseval"
 	"github.com/opencontainers/umoci/pkg/system"
-	"github.com/opencontainers/umoci/pkg/testutils"
 )
-
-// ignoreXattrs is a list of xattr names that should be ignored when
-// creating a new image layer, because they are host-specific and/or would be a
-// bad idea to unpack. They are also excluded from Lclearxattr when extracting
-// an archive.
-// XXX: Maybe we should make this configurable so users can manually blacklist
-//
-//	(or even whitelist) xattrs that they actually want included? Like how
-//	GNU tar's xattr setup works.
-var ignoreXattrs = map[string]struct{}{
-	// SELinux doesn't allow you to set SELinux policies generically. They're
-	// also host-specific. So just ignore them during extraction.
-	"security.selinux": {},
-
-	// NFSv4 ACLs are very system-specific and shouldn't be touched by us, nor
-	// should they be included in images.
-	"system.nfs4_acl": {},
-
-	// In order to support overlayfs whiteout mode, we shouldn't un-set
-	// this after we've set it when writing out the whiteouts.
-	"trusted.overlay.opaque": {},
-
-	// We don't want to these xattrs into the image, because they're only
-	// relevant based on how the build overlay is constructed and will not
-	// be true on the target system once the image is unpacked (e.g. inodes
-	// might be different, impure status won't be true, etc.).
-	"trusted.overlay.redirect": {},
-	"trusted.overlay.origin":   {},
-	"trusted.overlay.impure":   {},
-	"trusted.overlay.nlink":    {},
-	"trusted.overlay.upper":    {},
-	"trusted.overlay.metacopy": {},
-}
-
-func init() {
-	// For test purposes we add a fake forbidden attribute that an unprivileged
-	// user can easily write to (and thus we can test it).
-	if testutils.IsTestBinary() {
-		ignoreXattrs["user.UMOCI:forbidden_xattr"] = struct{}{}
-	}
-}
 
 // tarGenerator is a helper for generating layer diff tars. It should be noted
 // that when using tarGenerator.Add{Path,Whiteout} it is recommended to do it
@@ -212,33 +170,42 @@ func (tg *tarGenerator) AddFile(name, path string) error {
 	// Set up xattrs externally to updateHeader because the function signature
 	// would look really dumb otherwise.
 	// XXX: This should probably be moved to a function in tar_unix.go.
-	names, err := tg.fsEval.Llistxattr(path)
+	xattrs, err := tg.fsEval.Llistxattr(path)
 	if err != nil {
 		if !errors.Is(err, unix.EOPNOTSUPP) {
 			return fmt.Errorf("get xattr list: %w", err)
 		}
-		names = []string{}
+		xattrs = []string{}
 	}
-	for _, name := range names {
+	for _, xattr := range xattrs {
 		// Some xattrs need to be skipped for sanity reasons, such as
 		// security.selinux, because they are very much host-specific and
-		// carrying them to other hosts would be a really bad idea.
-		if _, ignore := ignoreXattrs[name]; ignore {
-			continue
+		// carrying them to other hosts would be a really bad idea. Other
+		// xattrs need to be remapped (such as escaped trusted.overlay.* xattrs
+		// when in overlayfs mode) to have correct values.
+		mappedName := xattr
+		if filter, isSpecial := getXattrFilter(xattr); isSpecial {
+			if newName := filter.ToTar(tg.whiteoutMode, xattr); newName == nil {
+				log.Debugf("xattr{%s} skipping the inclusion of xattr %q in generated tar archive", hdr.Name, xattr)
+				continue
+			} else if *newName != xattr {
+				mappedName = *newName
+				log.Debugf("xattr{%s} remapping xattr %q to %q in generated tar archive", hdr.Name, xattr, mappedName)
+			}
 		}
 		// TODO: We should translate all v3 capabilities into root-owned
 		//       capabilities here. But we don't have Go code for that yet
 		//       (we'd need to use libcap to parse it).
-		value, err := tg.fsEval.Lgetxattr(path, name)
+		value, err := tg.fsEval.Lgetxattr(path, xattr)
 		if err != nil {
 			// Ignore xattrs we were unable to read or if the filesystem is
 			// refusing to provide information about them. Note that rather
 			// than getting a permission error when reading a trusted.* xattr
 			// as an unprivileged user, you actually get ENODATA.
-			log.Debugf("failure reading xattr from list on %q: %q", name, err)
+			log.Debugf("xattr{%s} failure reading xattr %q: %v", hdr.Name, xattr, err)
 			// TODO: Should we use errors.As?
 			if !errors.Is(err, unix.EOPNOTSUPP) && !errors.Is(err, unix.ENODATA) {
-				return fmt.Errorf("get xattr: %s: %w", name, err)
+				return fmt.Errorf("get xattr %q: %w", xattr, err)
 			}
 		}
 		// https://golang.org/issues/20698 -- We don't just error out here
@@ -246,12 +213,12 @@ func (tg *tarGenerator) AddFile(name, path string) error {
 		// whether the stdlib will correctly handle reading or disable writing
 		// of these PAX headers so we have to track this ourselves.
 		if len(value) <= 0 {
-			log.Warnf("ignoring empty-valued xattr %s: disallowed by PAX standard", name)
+			log.Warnf("xattr{%s} ignoring empty-valued xattr %q: disallowed by PAX standard", hdr.Name, xattr)
 			continue
 		}
 		// Note that Go strings can actually be arbitrary byte sequences, so
 		// this conversion (while it might look a bit wrong) is actually fine.
-		hdr.Xattrs[name] = string(value)
+		hdr.Xattrs[mappedName] = string(value)
 	}
 
 	// Not all systems have the concept of an inode, but I'm not in the mood to

commit 54f34c91a8f0c69d565819ed5df92c77f5887459

oci: layer: refix auto-applied xattr handling
We've had special handling of xattrs like security.selinux (which the
system auto-applies when inodes are created) for a very long time, but
commit 9a1cefaf16bf ("oci: layer: correctly handle trusted.overlay xattr
namespace escaping") accidentally broke this during a refactor.

The solution is to simply invert the filter.MaskedOnDisk check, but we
need to have some tests that simulate security.selinux to ensure we
don't make the same mistake again in the future.

Fixes: 9a1cefaf16bf ("oci: layer: correctly handle trusted.overlay xattr namespace escaping")
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

oci/layer/tar_extract.go
@@ -581,7 +581,7 @@ func (te *TarExtractor) UnpackEntry(root string, hdr *tar.Header, r io.Reader) (
 						// being cleared. However, just to be safe we should
 						// verify that this is actually true (otherwise you'll
 						// end up with silently wrong extractions).
-						if filter.MaskedOnDisk(te.whiteoutMode, xattr) {
+						if !filter.MaskedOnDisk(te.whiteoutMode, xattr) {
 							// TODO: Find a nicer setup that doesn't require
 							// this fatal error.
 							log.Fatalf("[internal error] xattr{%s} masked %q is being hidden by (%T).GenerateEntry but UnpackShouldClear returns true", unsafeDir, xattr, filter)

oci/layer/xattr_linux_test.go
@@ -31,6 +31,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/vbatts/go-mtree"
+	"golang.org/x/sys/unix"
 
 	"github.com/opencontainers/umoci/pkg/fseval"
 	"github.com/opencontainers/umoci/pkg/system"
@@ -207,6 +208,123 @@ func TestUnpackGenerateRoundTrip_ComplexXattr_OverlayFS(t *testing.T) {
 					assert.Equalf(t, de.xattrs, xattrs, "UnpackEntry(%q): expected to see %#v not be remapped", path, de.xattrs)
 					assert.NotEqualf(t, de.remapXattrs, xattrs, "UnpackEntry(%q): expected to see %#v not be remapped", path, de.xattrs)
 				}
+			}
+
+			// We expect to get the exact same thing as the original archive
+			// entries in the new archive.
+			var wantDentries []tarDentry
+			for _, dentry := range dentries {
+				wantDentries = append(wantDentries, dentry.tarDentry)
+			}
+			testGenerateLayersForRoundTrip(t, dir, unpackOptions.WhiteoutMode, wantDentries)
+		})
+	}
+}
+
+func TestUnpackGenerateRoundTrip_MockedSELinux(t *testing.T) {
+	// For test purposes we add a fake forbidden attribute that an unprivileged
+	// user can easily write to (and thus we can test it). This is meant to be
+	// a stand-in for "security.selinux" or any other xattr that gets
+	// auto-applied and needs special handling with forbiddenXattrFilter{}.
+	const forbiddenTestXattr = "user.UMOCI.fake_selinux"
+	specialXattrs[forbiddenTestXattr] = forbiddenXattrFilter{}
+	defer delete(specialXattrs, forbiddenTestXattr)
+
+	// Make sure it actually is masked according to the filters.
+	filter, isSpecial := getXattrFilter(forbiddenTestXattr)
+	require.Truef(t, isSpecial, "getXattrFilter(%q) should return a filter", forbiddenTestXattr)
+	require.Equalf(t, forbiddenXattrFilter{}, filter, "getXattrFilter(%q) should return the forbidden filter", forbiddenTestXattr)
+	require.Truef(t, filter.MaskedOnDisk(OCIStandardWhiteout, forbiddenTestXattr), "getXattrFilter(%q).MaskedOnDisk should be true", forbiddenTestXattr)
+
+	dir := t.TempDir()
+
+	dentries := []struct {
+		tarDentry
+		autoXattrs map[string]string
+	}{
+		{
+			tarDentry{path: ".", ftype: tar.TypeDir, xattrs: map[string]string{
+				"user.dummy.xattr": "foobar",
+			}},
+			map[string]string{
+				forbiddenTestXattr: "rootdir",
+				// This should be auto-cleared because its not a masked xattr
+				// nor is it in the tar header.
+				"user.UMOCI.fake_nonmasked_xattr": "should get removed",
+			},
+		},
+		{
+			tarDentry{path: "foo/", ftype: tar.TypeDir, xattrs: map[string]string{
+				"user.dummy.xattr": "barbaz",
+			}},
+			map[string]string{
+				forbiddenTestXattr: "foodir",
+			},
+		},
+		{
+			tarDentry{path: "foo/bar", ftype: tar.TypeReg, contents: "file"},
+			map[string]string{
+				forbiddenTestXattr: "foobarfile",
+				// This should be auto-cleared because its not a masked xattr
+				// nor is it in the tar header.
+				"user.UMOCI.another_fake_nonmasked_xattr": "should also get removed",
+			},
+		},
+	}
+
+	for _, test := range []struct {
+		name   string
+		woType WhiteoutMode
+	}{
+		{"OverlayFSWhiteout", OverlayFSWhiteout},
+		{"OCIStandardWhiteout", OCIStandardWhiteout},
+	} {
+		test := test // copy iterator
+		t.Run(test.name, func(t *testing.T) {
+			unpackOptions := UnpackOptions{
+				MapOptions: MapOptions{
+					Rootless: os.Geteuid() != 0,
+				},
+				WhiteoutMode: test.woType,
+			}
+
+			te := NewTarExtractor(unpackOptions)
+
+			for _, de := range dentries {
+				hdr, rdr := tarFromDentry(de.tarDentry)
+				err := te.UnpackEntry(dir, hdr, rdr)
+				assert.NoErrorf(t, err, "UnpackEntry %s", hdr.Name)
+
+				// Apply the "auto" xattrs -- in order to make it seem like this
+				// was done automatically during extraction when the inode was
+				// created, we want to call applyMetadata here again to emulate
+				// this xattr being added by the system during UnpackEntry.
+				pth := filepath.Join(dir, de.path)
+				for xattr, value := range de.autoXattrs {
+					err := unix.Lsetxattr(pth, xattr, []byte(value), 0)
+					require.NoErrorf(t, err, "setxattr %s=%s on %q", xattr, value, hdr.Name)
+				}
+				err = te.restoreMetadata(pth, hdr)
+				require.NoErrorf(t, err, "restoreMetadata %s", hdr.Name)
+			}
+
+			for _, de := range dentries {
+				path := de.path
+				fullPath := filepath.Join(dir, path)
+
+				xattrs := getAllXattrs(t, fullPath)
+
+				wantXattrs := map[string]string{}
+				// We should see all of the hdr xattrs.
+				for xattr, value := range de.xattrs {
+					wantXattrs[xattr] = value
+				}
+				// Of the auto-applied xattrs, we only expect to see our dummy
+				// forbidden xattr after all the extractions.
+				if value, ok := de.autoXattrs[forbiddenTestXattr]; ok {
+					wantXattrs[forbiddenTestXattr] = value
+				}
+				assert.Equalf(t, wantXattrs, xattrs, "UnpackEntry(%q): expected to only see specific subset of applied xattrs", path)
 			}
 
 			// We expect to get the exact same thing as the original archive