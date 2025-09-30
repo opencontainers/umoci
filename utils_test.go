5be7a47750b6eba523128a3521222f1645bf91e3
Aleksa Sarai <cyphar@cyphar.com>
2025-10-01 03:31:28 +1000

    pprint: add tests for struct pprint
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

--- utils_test.go
+++ utils_test.go
@@ -22,6 +22,7 @@ import (
 	"bytes"
 	"testing"
 
+	ispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -158,6 +159,181 @@ func TestPprintSet(t *testing.T) {
 			var output bytes.Buffer
 			err := pprintSet(&output, test.prefix, test.key, test.data)
 			require.NoErrorf(t, err, "pprint(%q, %q, %v)", test.prefix, test.key, test.data)
+			assert.Equal(t, test.expected, output.String())
+		})
+	}
+}
+
+func TestPprintPlatform(t *testing.T) {
+	for _, test := range []struct {
+		name     string
+		prefix   string
+		platform ispec.Platform
+		expected string
+	}{
+		{"Basic", "\t\t", ispec.Platform{
+			OS:           "linux",
+			Architecture: "amd64",
+		}, "\t\t" + `Platform:
+			OS: linux
+			Architecture: amd64
+`},
+		{"ArchVariant", "\t\t", ispec.Platform{
+			OS:           "freebsd",
+			Architecture: "arm64",
+			Variant:      "v7",
+		}, "\t\t" + `Platform:
+			OS: freebsd
+			Architecture: arm64 (v7)
+`},
+		{"OSFeatures", "\t\t", ispec.Platform{
+			OS:           "windows",
+			OSVersion:    "10.0.14393.1066",
+			OSFeatures:   []string{"win32k", "win-dummy"},
+			Architecture: "amd64",
+		}, "\t\t" + `Platform:
+			OS: windows
+			OS Version: 10.0.14393.1066
+			OS Features: win32k, win-dummy
+			Architecture: amd64
+`},
+		{"Whitespace", "\t\t", ispec.Platform{
+			OS:           "fake os",
+			OSVersion:    "a b c",
+			OSFeatures:   []string{"a,b", "c d e f"},
+			Architecture: "arm is cool",
+			Variant:      "version 7",
+		}, "\t\t" + `Platform:
+			OS: "fake os"
+			OS Version: "a b c"
+			OS Features: "a,b", "c d e f"
+			Architecture: "arm is cool" (version 7)
+`},
+		{"Quoted", "\t\t", ispec.Platform{
+			OS:           "fake\tos\n",
+			OSVersion:    "a\vbc",
+			OSFeatures:   []string{"a\tb", "c\u00a0bar"},
+			Architecture: "arm\nis\ncool",
+			Variant:      "version\b7",
+		}, "\t\t" + `Platform:
+			OS: "fake\tos\n"
+			OS Version: "a\vbc"
+			OS Features: "a\tb", "c\u00a0bar"
+			Architecture: "arm\nis\ncool" ("version\b7")
+`},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			var output bytes.Buffer
+			err := pprintPlatform(&output, test.prefix, test.platform)
+			require.NoErrorf(t, err, "pprint(%q, %#v)", test.prefix, test.platform)
+			assert.Equal(t, test.expected, output.String())
+		})
+	}
+}
+
+func TestPprintDescriptor(t *testing.T) {
+	for _, test := range []struct {
+		name       string
+		prefix     string
+		descriptor ispec.Descriptor
+		expected   string
+	}{
+		{"Basic", "\t\t", ispec.Descriptor{
+			MediaType: "image/png",
+			Digest:    "sha256:7d30ef97fe167eb78d5c52503096522ebba7ba95789ba735fd68da9d4838f84d",
+			Size:      61235,
+		}, "\t\t" + `Descriptor:
+			Media Type: image/png
+			Digest: sha256:7d30ef97fe167eb78d5c52503096522ebba7ba95789ba735fd68da9d4838f84d
+			Size: 61.23kB
+`},
+		{"Annotations", "\t\t", ispec.Descriptor{
+			MediaType: ispec.MediaTypeImageManifest,
+			Digest:    "blake2:acbe05bb922855b8b2fea560e11ad2d135505a9f194a0fa99f1c57d3974990f6853ce22e97273d8dbf7424c09fe43e53ce4080dcc543482be46965bb24639cbe",
+			Size:      59129319,
+			Annotations: map[string]string{
+				"com.example.foo": "abc123",
+				"ci.umo.foobar":   "test",
+				"com.cyphar.test": "https://www.cyphar.com/",
+			},
+		}, "\t\t" + `Descriptor:
+			Media Type: application/vnd.oci.image.manifest.v1+json
+			Digest: blake2:acbe05bb922855b8b2fea560e11ad2d135505a9f194a0fa99f1c57d3974990f6853ce22e97273d8dbf7424c09fe43e53ce4080dcc543482be46965bb24639cbe
+			Size: 59.13MB
+			Annotations:
+				ci.umo.foobar: test
+				com.cyphar.test: https://www.cyphar.com/
+				com.example.foo: abc123
+`},
+		{"URLs", "\t\t", ispec.Descriptor{
+			MediaType: ispec.MediaTypeImageLayerGzip,
+			Digest:    "sha512:dd65b6a8d277c7b06003178fc7a125fa6ad021dff2beffd82df0b469f2afe735312e2932608d42d1bc8421141edb0882680e36d45f5d804783ef555a3566a9f8",
+			Size:      926812731,
+			URLs: []string{
+				"https://www.example.com/example.txt",
+				"https://www.cyphar.com/",
+			},
+		}, "\t\t" + `Descriptor:
+			Media Type: application/vnd.oci.image.layer.v1.tar+gzip
+			Digest: sha512:dd65b6a8d277c7b06003178fc7a125fa6ad021dff2beffd82df0b469f2afe735312e2932608d42d1bc8421141edb0882680e36d45f5d804783ef555a3566a9f8
+			Size: 926.8MB
+			URLs:
+				https://www.example.com/example.txt
+				https://www.cyphar.com/
+`},
+		{"Whitespace", "\t\t", ispec.Descriptor{
+			MediaType: "application/x-dummy; foo",
+			Digest:    "dummy: a b c d",
+			Size:      123,
+			URLs: []string{
+				"https:// www. example.com/example.txt",
+				"https:// www. cyphar.com/",
+			},
+			Annotations: map[string]string{
+				"com. example.foo": "abc123",
+				"ci.umo.foobar":    "test foo",
+				"com cyphar test":  "https   www cyphar com ",
+			},
+		}, "\t\t" + `Descriptor:
+			Media Type: "application/x-dummy; foo"
+			Digest: "dummy: a b c d"
+			Size: 123B
+			URLs:
+				"https:// www. example.com/example.txt"
+				"https:// www. cyphar.com/"
+			Annotations:
+				ci.umo.foobar: "test foo"
+				"com cyphar test": "https   www cyphar com "
+				"com. example.foo": abc123
+`},
+		{"Quoted", "\t\t", ispec.Descriptor{
+			MediaType: "application/x-dummy;\nfoo",
+			Digest:    "dummy:a\bb\bc\td",
+			Size:      123,
+			URLs: []string{
+				"\t\n",
+			},
+			Annotations: map[string]string{
+				"com.\texample.foo": "abc123",
+				"ci.umo.foobar":     "test\nfoo",
+				"com cyphar test":   "https \t www cyphar com ",
+			},
+		}, "\t\t" + `Descriptor:
+			Media Type: "application/x-dummy;\nfoo"
+			Digest: "dummy:a\bb\bc\td"
+			Size: 123B
+			URLs:
+				"\t\n"
+			Annotations:
+				ci.umo.foobar: "test\nfoo"
+				"com cyphar test": "https \t www cyphar com "
+				"com.\texample.foo": abc123
+`},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			var output bytes.Buffer
+			err := pprintDescriptor(&output, test.prefix, test.descriptor)
+			require.NoErrorf(t, err, "pprint(%q, %#v)", test.prefix, test.descriptor)
 			assert.Equal(t, test.expected, output.String())
 		})
 	}
