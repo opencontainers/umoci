opencontainers/umoci

commit 4a024376584598e9773ac0491c297993c929a150

casext: provide raw blob data when fetching parsable blobs
We have to read the whole blob into memory anyway and some users might
want to use the raw blob data rather than our re-parsed version (i.e.,
there may be JSON fields that get ignored when we parse them).

Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

oci/casext/blob.go
@@ -19,6 +19,7 @@
 package casext
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
@@ -36,6 +37,10 @@ type Blob struct {
 	// Descriptor is the {mediatype,digest,length} 3-tuple. Note that this
 	// isn't updated if the Data is modified.
 	Descriptor ispec.Descriptor
+
+	// RawData is the un-parsed blob data taken from the OCI image's blob
+	// store. If the data is unparseable, this value will be nil.
+	RawData []byte
 
 	// Data is the "parsed" blob taken from the OCI image's blob store, and is
 	// typed according to the media type. The default mappings from MIME =>
@@ -62,28 +67,39 @@ func (b *Blob) Close() error {
 }
 
 // FromDescriptor parses the blob referenced by the given descriptor.
-func (e Engine) FromDescriptor(ctx context.Context, descriptor ispec.Descriptor) (_ *Blob, Err error) {
+func (e Engine) FromDescriptor(ctx context.Context, descriptor ispec.Descriptor) (blob *Blob, Err error) {
 	reader, err := e.GetVerifiedBlob(ctx, descriptor)
 	if err != nil {
 		return nil, fmt.Errorf("get blob: %w", err)
 	}
 
-	blob := Blob{
+	blob = &Blob{
 		Descriptor: descriptor,
 		Data:       reader,
 	}
 
 	if fn := mediatype.GetParser(descriptor.MediaType); fn != nil {
+		rawDataBuf := new(bytes.Buffer)
+		dataReader := io.TeeReader(reader, rawDataBuf)
+
 		defer func() {
-			if _, err := system.Copy(io.Discard, reader); Err == nil && err != nil {
+			if _, err := system.Copy(io.Discard, dataReader); Err == nil && err != nil {
 				Err = fmt.Errorf("discard trailing %q blob: %w", descriptor.MediaType, err)
 			}
 			if err := reader.Close(); Err == nil && err != nil {
 				Err = fmt.Errorf("close %q blob: %w", descriptor.MediaType, err)
 			}
+			if blob != nil {
+				// Include all trailing data.
+				blob.RawData = rawDataBuf.Bytes()
+				if len(blob.RawData) <= 0 && Err == nil {
+					// Sanity check.
+					Err = fmt.Errorf("parsing %q blob succeeded but raw data has no length", descriptor.MediaType)
+				}
+			}
 		}()
 
-		data, err := fn(reader)
+		data, err := fn(dataReader)
 		if err != nil {
 			return nil, fmt.Errorf("parse %s: %w", descriptor.MediaType, err)
 		}
@@ -92,5 +108,5 @@ func (e Engine) FromDescriptor(ctx context.Context, descriptor ispec.Descriptor)
 	if blob.Data == nil {
 		return nil, errors.New("[internal error] b.Data was nil after parsing")
 	}
-	return &blob, nil
+	return blob, nil
 }

commit de8512a5f35f8dd61152405364eddfec892f4a7a

casext: improve Blob.RawData sanity check
The sanity check for Blob.RawData really should verify that the data is
the correct size, as opposed to just >0. In addition, this is a
programmer error and so should cause a panic.

Fixes: 4a0243765845 ("casext: provide raw blob data when fetching parsable blobs")
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

oci/casext/blob.go
@@ -27,6 +27,7 @@ import (
 
 	ispec "github.com/opencontainers/image-spec/specs-go/v1"
 
+	"github.com/opencontainers/umoci/internal/assert"
 	"github.com/opencontainers/umoci/internal/system"
 	"github.com/opencontainers/umoci/oci/casext/mediatype"
 )
@@ -92,10 +93,10 @@ func (e Engine) FromDescriptor(ctx context.Context, descriptor ispec.Descriptor)
 			if blob != nil {
 				// Include all trailing data.
 				blob.RawData = rawDataBuf.Bytes()
-				if len(blob.RawData) <= 0 && Err == nil {
-					// Sanity check.
-					Err = fmt.Errorf("parsing %q blob succeeded but raw data has no length", descriptor.MediaType)
-				}
+				// Sanity check.
+				assert.Assertf(int64(len(blob.RawData)) == descriptor.Size,
+					"parsing %q blob succeeded but raw data has unexpected length (expected %d bytes but got %d bytes)",
+					descriptor.MediaType, descriptor.Size, len(blob.RawData))
 			}
 		}()
 